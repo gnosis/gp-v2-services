e491d0979054a50feea85cd0c9133b1126ecd682
Valentin <vakevk+git@gmail.com>
2020-12-09 13:06:40 +0100

    Set allow origin header so that openapi works with localhost
    We're doing the same thing for the v1 price estimator.

--- orderbook/src/main.rs
+++ orderbook/src/main.rs
@@ -3,12 +3,14 @@ mod orderbook;
 
 use crate::orderbook::OrderBook;
 use std::{net::SocketAddr, sync::Arc};
+use warp::Filter;
 
 #[tokio::main]
 async fn main() {
     tracing_subscriber::fmt::init();
     let orderbook = Arc::new(OrderBook::default());
-    let filter = api::handle_all_routes(orderbook);
+    let filter = api::handle_all_routes(orderbook)
+        .map(|reply| warp::reply::with_header(reply, "Access-Control-Allow-Origin", "*"));
     let address = SocketAddr::new([0, 0, 0, 0].into(), 8080);
     tracing::info!(%address, "serving order book");
     warp::serve(filter).bind(address).await;
