fffad3bc41f60faf324e9a0b019f212d1c3fb620
Valentin <vakevk+git@gmail.com>
2020-12-09 12:21:08 +0100

    Setup tracing in shared crate
    with the proper log separator timestamp and panic handler.

--- orderbook/src/main.rs
+++ orderbook/src/main.rs
@@ -6,7 +6,7 @@ use std::{net::SocketAddr, sync::Arc};
 
 #[tokio::main]
 async fn main() {
-    tracing_subscriber::fmt::init();
+    tracing_setup::initialize("WARN,orderbook=DEBUG");
     let orderbook = Arc::new(OrderBook::default());
     let filter = api::handle_all_routes(orderbook);
     let address = SocketAddr::new([0, 0, 0, 0].into(), 8080);
