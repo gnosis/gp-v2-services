94b342bb7afb3bd968f07d1d812eef366927e866
Felix Leupold <felixleupold90@gmail.com>
2021-01-26 14:57:30 +0100

    comments

--- solver/src/liquidity.rs
+++ solver/src/liquidity.rs
@@ -1,4 +1,4 @@
-use model::order::OrderKind;
+use model::{order::OrderKind, TokenPair};
 use num_rational::Rational;
 use primitive_types::{H160, U256};
 use settlement::{Interaction, Trade};
@@ -9,16 +9,12 @@ use mockall::automock;
 
 use crate::settlement;
 
-///
 /// Defines the different types of liquidity our solvers support
-///
 pub enum Liquidity {
     Limit(LimitOrder),
     Amm(AmmOrder),
 }
-///
 /// Basic limit sell and buy orders
-///
 #[derive(Clone)]
 pub struct LimitOrder {
     pub sell_token: H160,
@@ -30,28 +26,22 @@ pub struct LimitOrder {
     pub settlement_handling: Arc<dyn LimitOrderSettlementHandling>,
 }
 
-///
 /// Specifies how a limit order fulfillment translates into Trade and Interactions for the settlement
-///
 #[cfg_attr(test, automock)]
 pub trait LimitOrderSettlementHandling: Send + Sync {
     fn settle(&self, executed_amount: U256) -> (Option<Trade>, Vec<Box<dyn Interaction>>);
 }
 
-///
 /// 2 sided constant product automated market maker with equal reserve value and a trading fee (e.g. Uniswap, Sushiswap)
-///
 #[derive(Clone)]
 pub struct AmmOrder {
-    pub tokens: (H160, H160),
+    pub tokens: TokenPair,
     pub reserves: (u128, u128),
     pub fee: Rational,
     pub settlement_handling: Arc<dyn AmmSettlementHandling>,
 }
 
-///
 /// Specifies how a AMM order fulfillment translates into Interactions for the settlement
-///
 #[cfg_attr(test, automock)]
 pub trait AmmSettlementHandling: Send + Sync {
     fn settle(&self, input: (H160, U256), output: (H160, U256)) -> Vec<Box<dyn Interaction>>;
