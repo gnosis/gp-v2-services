cc9d6c465a149f1d3ad3c73ad7e60624e906be87
Valentin <77051586+vkgnosis@users.noreply.github.com>
2021-02-22 15:27:24 +0100

    Make block stream clonable
    so that we only poll in one place and share the result with all streams.

--- shared/src/current_block.rs
+++ shared/src/current_block.rs
@@ -1,7 +1,8 @@
 use anyhow::{anyhow, Context, Result};
-use futures::{stream::FusedStream, Stream};
+use futures::Stream;
 use primitive_types::H256;
-use std::time::Duration;
+use std::{future::Future, time::Duration};
+use tokio::sync::watch;
 use web3::{
     types::{BlockId, BlockNumber},
     Transport, Web3,
@@ -11,40 +12,59 @@ pub type Block = web3::types::Block<H256>;
 
 const POLL_INTERVAL: Duration = Duration::from_secs(1);
 
-/// A stream that yields the current block whenever it changes. This is not guaranteed to yield
-/// *every* block individually without gaps but it does yield the newest block whenever it changes.
-/// In practice this means that if the node changes the current block in quick succession we might
-/// only observe the last block, skipping some blocks in between.
-pub fn block_stream(web3: Web3<impl Transport>) -> impl Stream<Item = Block> + FusedStream {
-    // We do not use a block filter because filters have been unreliable in the past, do not work
-    // with load balancing over multiple nodes and would still need to be polled unless we used web
-    // sockets.
-    futures::stream::unfold(H256::zero(), move |previous| {
-        let web3 = web3.clone();
-        async move {
-            loop {
-                tokio::time::delay_for(POLL_INTERVAL).await;
-                let block = match current_block(&web3).await {
-                    Ok(block) => block,
-                    Err(err) => {
-                        tracing::warn!("failed to get current block: {:?}", err);
-                        continue;
-                    }
-                };
-                let hash = match block.hash {
-                    Some(hash) => hash,
-                    None => {
-                        tracing::warn!("missing hash");
-                        continue;
-                    }
-                };
-                if hash == previous {
+/// Creates a clonable stream that yields the current block whenever it changes and a future
+/// driving the updates of current block.
+///
+/// The stream is not guaranteed to yield *every* block individually without gaps but it does yield
+/// the newest block whenever it changes. In practice this means that if the node changes the
+/// current block in quick succession we might only observe the last block, skipping some blocks in
+/// between. If the the current block has not yet been fetched the first time the stream yields a
+/// default block with hash and number 0.
+///
+/// The future runs until all associated streams have been dropped.
+pub fn current_block_stream(
+    web3: Web3<impl Transport>,
+) -> (
+    impl Stream<Item = Block> + Clone + Unpin,
+    impl Future<Output = ()>,
+) {
+    let default_hash = H256::default();
+    let default_block = Block {
+        hash: Some(default_hash),
+        number: Some(0.into()),
+        ..Default::default()
+    };
+    let (sender, receiver) = watch::channel(default_block);
+
+    let update_future = async move {
+        let mut previous_hash = default_hash;
+        loop {
+            tokio::time::delay_for(POLL_INTERVAL).await;
+            let block = match current_block(&web3).await {
+                Ok(block) => block,
+                Err(err) => {
+                    tracing::warn!("failed to get current block: {:?}", err);
                     continue;
                 }
-                return Some((block, hash));
+            };
+            let hash = match block.hash {
+                Some(hash) => hash,
+                None => {
+                    tracing::warn!("missing hash");
+                    continue;
+                }
+            };
+            if hash == previous_hash {
+                continue;
             }
+            if sender.broadcast(block).is_err() {
+                break;
+            }
+            previous_hash = hash;
         }
-    })
+    };
+
+    (receiver, update_future)
 }
 
 async fn current_block(web3: &Web3<impl Transport>) -> Result<Block> {
@@ -67,10 +87,14 @@ mod tests {
         let node = "https://dev-openethereum.mainnet.gnosisdev.com";
         let transport = web3::transports::Http::new(node).unwrap();
         let web3 = Web3::new(transport);
-        let stream = block_stream(web3);
-        futures::pin_mut!(stream);
-        while let Some(block) = stream.next().await {
+        let (mut stream, future) = current_block_stream(web3);
+        let handle = tokio::task::spawn(future);
+        for _ in 0..3 {
+            let block = stream.next().await.unwrap();
             println!("new block number {}", block.number.unwrap().as_u64());
         }
+        println!("dropping stream which should cause the future to exit");
+        std::mem::drop(stream);
+        handle.await.unwrap();
     }
 }
