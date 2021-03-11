7b45544ad26942b2a81e0607808fe532e6387a48
Felix Leupold <felixleupold90@gmail.com>
2021-03-11 12:47:55 +0100

    rebase

--- orderbook/src/fee.rs
+++ orderbook/src/fee.rs
@@ -84,22 +84,6 @@ impl MinFeeCalculator {
         let official_valid_until = now + Duration::seconds(STANDARD_VALIDITY_FOR_FEE_IN_SEC);
         let internal_valid_until = now + Duration::seconds(PERSISTED_VALIDITY_FOR_FEE_IN_SEC);
 
-        let sell_amount =
-            if let (Some(buy_token), Some(amount), Some(kind)) = (buy_token, amount, kind) {
-                Some(round_amount(
-                    get_sell_amount(
-                        sell_token,
-                        buy_token,
-                        amount,
-                        kind,
-                        self.price_estimator.as_ref(),
-                    )
-                    .await?,
-                ))
-            } else {
-                None
-            };
-
         if let Ok(Some(past_fee)) = self
             .measurements
             .get_min_fee(sell_token, buy_token, amount, kind, official_valid_until)
@@ -166,29 +150,6 @@ impl MinFeeCalculator {
         }
         false
     }
-}
-
-async fn get_sell_amount(
-    sell_token: H160,
-    buy_token: H160,
-    amount: U256,
-    kind: OrderKind,
-    price_estimator: &dyn PriceEstimating,
-) -> Result<U256> {
-    Ok(match kind {
-        OrderKind::Buy => U256::from_f64_lossy(
-            amount.to_f64_lossy()
-                * price_estimator
-                    .estimate_price(sell_token, buy_token)
-                    .await?,
-        ),
-        OrderKind::Sell => amount,
-    })
-}
-
-fn round_amount(amount: U256) -> U256 {
-    let order_of_magnitude = amount.to_string().len() - 1;
-    U256::exp10(order_of_magnitude) * 5
 }
 
 struct FeeMeasurement {
@@ -353,49 +314,5 @@ mod tests {
         // Gas price reduces, and slightly lower fee is now valid
         *gas_price.lock().unwrap() /= 2.0;
         assert!(fee_estimator.is_valid_fee(token, lower_fee).await);
-    }
-
-    #[tokio::test]
-    async fn test_sell_amount() {
-        let price_estimator = Box::new(FakePriceEstimator(10.0));
-        let sell_token = H160::from_low_u64_be(1);
-        let buy_token = H160::from_low_u64_be(2);
-        let amount = U256::exp10(18);
-
-        let sell_amount = get_sell_amount(
-            sell_token,
-            buy_token,
-            amount,
-            OrderKind::Sell,
-            price_estimator.as_ref(),
-        )
-        .await
-        .unwrap();
-        assert_eq!(sell_amount, amount);
-
-        let sell_amount = get_sell_amount(
-            sell_token,
-            buy_token,
-            amount,
-            OrderKind::Buy,
-            price_estimator.as_ref(),
-        )
-        .await
-        .unwrap();
-        assert_eq!(sell_amount, amount * 10);
-    }
-
-    #[test]
-    fn test_round_amount() {
-        assert_eq!(round_amount(0.into()), 5.into());
-        assert_eq!(round_amount(1.into()), 5.into());
-        assert_eq!(round_amount(9.into()), 5.into());
-        assert_eq!(round_amount(10.into()), 50.into());
-        assert_eq!(round_amount(100.into()), 500.into());
-
-        assert_eq!(
-            round_amount(U256::exp10(18)),
-            5_000_000_000_000_000_000u128.into()
-        );
     }
 }
