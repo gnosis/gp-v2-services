132be84ca286f95c1d2620a161b7ae2b732aa297
Ben Smith <bh2smith@gmail.com>
2021-03-16 09:06:39 +0100

    tests for cancel_order_response

--- orderbook/src/api/cancel_order.rs
+++ orderbook/src/api/cancel_order.rs
@@ -1,29 +1,20 @@
+use crate::api::extract_payload;
 use crate::orderbook::{OrderCancellationResult, Orderbook};
 use anyhow::Result;
 use model::order::OrderCancellation;
 use std::{convert::Infallible, sync::Arc};
 use warp::{hyper::StatusCode, Filter, Rejection, Reply};
 
-const MAX_JSON_BODY_PAYLOAD: u64 = 1024 * 16;
-
-fn extract_cancellation() -> impl Filter<Extract = (OrderCancellation,), Error = Rejection> + Clone
-{
-    // (rejecting huge payloads)...
-    warp::body::content_length_limit(MAX_JSON_BODY_PAYLOAD).and(warp::body::json())
-}
-
 pub fn cancel_order_request(
 ) -> impl Filter<Extract = (OrderCancellation,), Error = Rejection> + Clone {
     warp::path!("orders")
         .and(warp::delete())
-        .and(extract_cancellation())
+        .and(extract_payload())
 }
 
 pub fn cancel_order_response(result: Result<OrderCancellationResult>) -> impl Reply {
     let (body, status_code) = match result {
-        Ok(OrderCancellationResult::Cancelled) => {
-            (warp::reply::json(&"Cancelled"), StatusCode::OK)
-        }
+        Ok(OrderCancellationResult::Cancelled) => (warp::reply::json(&"Cancelled"), StatusCode::OK),
         Ok(OrderCancellationResult::InvalidSignature) => (
             super::error("InvalidSignature", "Likely malformed signature"),
             StatusCode::BAD_REQUEST,
@@ -75,5 +66,30 @@ mod tests {
             .json(&cancellation);
         let result = request.filter(&filter).await.unwrap();
         assert_eq!(result, cancellation);
+    }
+
+    #[tokio::test]
+    async fn cancel_order_response_ok() {
+        let response =
+            cancel_order_response(Ok(OrderCancellationResult::Cancelled)).into_response();
+        assert_eq!(response.status(), StatusCode::OK);
+    }
+
+    #[tokio::test]
+    async fn cancel_order_response_err() {
+        let response =
+            cancel_order_response(Ok(OrderCancellationResult::InvalidSignature)).into_response();
+        assert_eq!(response.status(), StatusCode::BAD_REQUEST);
+
+        let response =
+            cancel_order_response(Ok(OrderCancellationResult::WrongOwner)).into_response();
+        assert_eq!(response.status(), StatusCode::UNAUTHORIZED);
+
+        let response =
+            cancel_order_response(Ok(OrderCancellationResult::OrderNotFound)).into_response();
+        assert_eq!(response.status(), StatusCode::NOT_FOUND);
+
+        let response = cancel_order_response(Err(anyhow::Error::msg("test error"))).into_response();
+        assert_eq!(response.status(), StatusCode::INTERNAL_SERVER_ERROR);
     }
 }
