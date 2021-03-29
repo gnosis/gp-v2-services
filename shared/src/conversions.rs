f3cb40db165952d71fcf2eb6419d0700c9491b65
Felix Leupold <felixleupold90@gmail.com>
2021-03-29 17:36:20 +0200

    [NaiveSolver] Using BigRational in in/out computation to avoid rounding issues

--- shared/src/conversions.rs
+++ shared/src/conversions.rs
@@ -1,10 +1,18 @@
-use num::bigint::Sign;
+use anyhow::{anyhow, Result};
 use num::BigInt;
+use num::{bigint::Sign, Zero};
 use num::{BigRational, ToPrimitive as _};
 use primitive_types::U256;
 
 pub fn big_rational_to_float(ratio: &BigRational) -> Option<f64> {
     Some(ratio.numer().to_f64()? / ratio.denom().to_f64()?)
+}
+
+pub fn big_rational_to_u256(ratio: &BigRational) -> Result<U256> {
+    if ratio.denom() == &BigInt::zero() {
+        return Err(anyhow!(" Division by 0 in BigRational to U256 conversion"));
+    }
+    big_int_to_u256(&(ratio.numer() / ratio.denom()))
 }
 
 pub fn u256_to_big_int(input: &U256) -> BigInt {
@@ -16,6 +24,17 @@ pub fn u256_to_big_int(input: &U256) -> BigInt {
 pub fn u256_to_big_rational(input: &U256) -> BigRational {
     let as_bigint = u256_to_big_int(input);
     BigRational::new(as_bigint, 1.into())
+}
+
+pub fn big_int_to_u256(input: &BigInt) -> Result<U256> {
+    let (sign, bytes) = input.to_bytes_be();
+    if sign == Sign::Minus {
+        return Err(anyhow!("Negative BigInt to U256 conversion"));
+    }
+    if bytes.len() > 32 {
+        return Err(anyhow!("BigInt too big for U256 conversion"));
+    }
+    Ok(U256::from_big_endian(&bytes))
 }
 
 // Convenience:
