1873c927bbad61c6ab3534fecbdf78db5f639272
Ben Smith <bh2smith@gmail.com>
2021-05-31 14:09:35 +0200

    include some empty methods for intended functionality

--- shared/src/balancer_event_handler.rs
+++ shared/src/balancer_event_handler.rs
@@ -3,6 +3,7 @@ use crate::{
     event_handling::{BlockNumber, EventHandler, EventIndex, EventStoring},
     impl_event_retrieving,
     maintenance::Maintaining,
+    Web3,
 };
 use anyhow::{anyhow, Context, Result};
 use contracts::{
@@ -13,8 +14,11 @@ use contracts::{
 };
 use ethcontract::common::DeploymentInformation;
 use ethcontract::{dyns::DynWeb3, Event as EthContractEvent, EventMetadata, H160, H256};
-use std::fmt::Debug;
-use std::ops::RangeInclusive;
+use std::{
+    collections::{HashMap, HashSet},
+    fmt::Debug,
+    ops::RangeInclusive,
+};
 use tokio::sync::Mutex;
 
 #[derive(Debug)]
@@ -27,6 +31,21 @@ pub struct PoolRegistered {
     pub pool_id: H256,
     pub pool_address: H160,
     pub specialization: PoolSpecialization,
+}
+
+#[derive(Clone, Default, Eq, PartialEq, Hash)]
+pub struct WeightedPool {
+    pool_address: H160,
+    tokens: Vec<H160>,
+    pool_id: H256,
+}
+
+#[derive(Default)]
+pub struct BalancerPools {
+    _pools_by_token: HashMap<H160, HashSet<H256>>,
+    pools: HashMap<H256, WeightedPool>,
+    // Block number of last update
+    last_updated: u64,
 }
 
 /// There are three specialization settings for Pools,
@@ -59,10 +78,69 @@ impl PoolSpecialization {
     }
 }
 
-#[derive(Default)]
-pub struct BalancerPools {}
-
 impl BalancerPools {
+    fn _update_last_block(mut self, value: u64) {
+        self.last_updated = value;
+    }
+
+    // All insertions happen in one transaction.
+    fn insert_events(&self, events: Vec<(EventIndex, BalancerEvent)>) -> Result<()> {
+        for (index, event) in events {
+            match event {
+                BalancerEvent::PoolRegistered(event) => self.insert_pool(index, event),
+            };
+        }
+        Ok(())
+    }
+
+    fn _delete_events(&self, _delete_from_block_number: u64) -> Result<()> {
+        // TODO - delete from when asked.
+        Ok(())
+    }
+
+    fn replace_events(
+        &self,
+        _delete_from_block_number: u64,
+        events: Vec<(EventIndex, BalancerEvent)>,
+    ) -> Result<()> {
+        // self.delete_events(delete_from_block_number)?;
+        self.insert_events(events)?;
+        Ok(())
+    }
+
+    fn known_pool(&self, pool_id: H256) -> bool {
+        self.pools.contains_key(&pool_id)
+    }
+
+    fn insert_pool(&self, index: EventIndex, registration: PoolRegistered) {
+        if !self.known_pool(registration.pool_id) {
+            let pool_tokens = vec![];
+            let _weighted_pool = WeightedPool {
+                pool_address: registration.pool_address,
+                pool_id: registration.pool_id,
+                tokens: pool_tokens,
+            };
+            // Need to figure out a way to update this.
+            // self.pools
+            //     .entry(registration.pool_id)
+            //     .or_default()
+            //     .insert(weighted_pool.clone());
+            // for token in pool_tokens {
+            //     self.pools_by_token
+            //         .entry(token)
+            //         .or_default()
+            //         .insert(weighted_pool.pool_id);
+            // }
+            tracing::debug!(
+                "Updated Balancer Pools with {:?} - {:?}",
+                registration.pool_address,
+                index
+            );
+        } else {
+            tracing::debug!("Ignored known pool {:?}", registration.pool_address);
+        }
+    }
+
     fn contract_to_balancer_events(
         &self,
         contract_events: Vec<EthContractEvent<ContractEvent>>,
@@ -78,8 +156,16 @@ impl BalancerPools {
                     ContractEvent::PoolRegistered(event) => {
                         Some(convert_pool_registered(&event, &meta))
                     }
+                    // ContractEvent::TokensDeregistered(event) => {
+                    //     tracing::debug!("Tokens Deregistered {:?}", event);
+                    //     None
+                    // }
+                    // ContractEvent::TokensRegistered(event) => {
+                    //     tracing::debug!("Tokens Registered {:?}", event);
+                    //     None
+                    // }
                     _ => {
-                        tracing::info!("Got {:?}", data);
+                        // TODO - Not processing other events at the moment.
                         None
                     }
                 }
@@ -88,29 +174,50 @@ impl BalancerPools {
     }
 }
 
+pub struct BalancerPoolFetcher {
+    pub web3: Web3,
+}
+
+impl BalancerPoolFetcher {
+    async fn _get_pool_tokens(&self, _pool_address: H160) -> Vec<H160> {
+        // let web3 = Web3::new(self.web3.transport().clone());
+        // let pool_contract = BalancerPool::at(&web3, pool_address).await;
+        // TODO - fetch tokens from pool
+        // There are two different types of pools, hopefully they share a common interface.
+        vec![]
+    }
+}
+
 pub struct BalancerEventUpdater(
     Mutex<EventHandler<DynWeb3, BalancerV2VaultContract, BalancerPools>>,
 );
 
 impl BalancerEventUpdater {
-    pub async fn new(contract: BalancerV2Vault, pools: BalancerPools) -> Result<Self> {
+    pub async fn new(contract: BalancerV2Vault, pools: BalancerPools) -> Self {
         let deployment_block = match contract.deployment_information() {
             Some(DeploymentInformation::BlockNumber(block_number)) => Some(block_number),
-            Some(DeploymentInformation::TransactionHash(hash)) => Some(
-                contract
+            Some(DeploymentInformation::TransactionHash(hash)) => {
+                match contract
                     .raw_instance()
                     .web3()
                     .block_number_from_tx_hash(hash)
-                    .await?,
-            ),
+                    .await
+                {
+                    Ok(block_number) => Some(block_number),
+                    Err(err) => {
+                        tracing::warn!("no deployment block for hash {}: {}", hash, err);
+                        None
+                    }
+                }
+            }
             None => None,
         };
-        Ok(Self(Mutex::new(EventHandler::new(
+        Self(Mutex::new(EventHandler::new(
             contract.raw_instance().web3(),
             BalancerV2VaultContract(contract),
             pools,
             deployment_block,
-        ))))
+        )))
     }
 }
 
@@ -129,19 +236,21 @@ impl EventStoring<ContractEvent> for BalancerPools {
             balancer_events.len(),
             range.start().to_u64()
         );
-        todo!()
+        // Not sure if we even need this... since balancer team claims there will never be deregistered pools
+        // However, it is still possible.
+        self.replace_events(0, balancer_events)?;
+        Ok(())
     }
 
     async fn append_events(&self, events: Vec<EthContractEvent<ContractEvent>>) -> Result<()> {
         let balancer_events = self
             .contract_to_balancer_events(events)
             .context("failed to convert events")?;
-        tracing::debug!("inserting {} new events", balancer_events.len());
-        todo!()
+        self.insert_events(balancer_events)
     }
 
     async fn last_event_block(&self) -> Result<u64> {
-        todo!()
+        Ok(self.last_updated)
     }
 }
 
