bfb498414eb2d8a38cf31c27ce55f1543b4c9580
Ben Smith <bh2smith@gmail.com>
2021-06-02 15:05:53 +0200

    remove changes to main

--- orderbook/src/main.rs
+++ orderbook/src/main.rs
@@ -1,4 +1,4 @@
-use contracts::{BalancerV2Vault, GPv2Settlement, WETH9};
+use contracts::{GPv2Settlement, WETH9};
 use model::{
     order::{OrderUid, BUY_ETH_ADDRESS},
     DomainSeparator,
@@ -14,7 +14,6 @@ use orderbook::{
 };
 use primitive_types::H160;
 use prometheus::Registry;
-use shared::balancer_event_handler::{BalancerEventUpdater, BalancerPools};
 use shared::{
     bad_token::{
         cache::CachingDetector,
@@ -131,9 +130,6 @@ async fn main() {
     let settlement_contract = GPv2Settlement::deployed(&web3)
         .await
         .expect("Couldn't load deployed settlement");
-    let vault_contract = BalancerV2Vault::deployed(&web3)
-        .await
-        .expect("Couldn't load deployed balancer vault");
     let gp_allowance = settlement_contract
         .allowance_manager()
         .call()
@@ -168,8 +164,6 @@ async fn main() {
 
     let event_updater =
         EventUpdater::new(settlement_contract.clone(), database.clone(), sync_start);
-    let balancer_event_updater =
-        BalancerEventUpdater::new(vault_contract, BalancerPools::default()).await;
     let balance_fetcher =
         Web3BalanceFetcher::new(web3.clone(), gp_allowance, settlement_contract.address());
 
@@ -252,7 +246,6 @@ async fn main() {
             orderbook.clone(),
             Arc::new(database.clone()),
             Arc::new(event_updater),
-            Arc::new(balancer_event_updater),
         ],
     };
     check_database_connection(orderbook.as_ref()).await;
