e81a2c0b187d3ce0d224504183b5862f407c9565
Ben Smith <bh2smith@gmail.com>
2021-06-08 11:30:17 +0200

    move Weighted Pool contract to vendor abi only

--- contracts/src/bin/vendor.rs
+++ contracts/src/bin/vendor.rs
@@ -30,10 +30,6 @@ fn run() -> Result<()> {
             "BalancerV2Vault",
             "balancer-labs/balancer-v2-monorepo/a3b570a2aa655d4c4941a67e3db6a06fbd72ef09/pkg/deployments/deployed/mainnet/Vault.json",
         )?
-        .github(
-            "BalancerV2WeightedPool",
-            "balancer-labs/balancer-v2-monorepo/a3b570a2aa655d4c4941a67e3db6a06fbd72ef09/pkg/deployments/extra-abis/WeightedPool.json",
-        )?
         .npm(
             "ERC20Mintable",
             "@openzeppelin/contracts@2.5.0/build/contracts/ERC20Mintable.json",
@@ -61,6 +57,10 @@ fn run() -> Result<()> {
 
     vendor
         .abi_only()
+        .github(
+            "BalancerV2WeightedPool",
+            "balancer-labs/balancer-v2-monorepo/a3b570a2aa655d4c4941a67e3db6a06fbd72ef09/pkg/deployments/extra-abis/WeightedPool.json",
+        )?
         .npm(
             "ERC20",
             "@openzeppelin/contracts@3.3.0/build/contracts/ERC20.json",
