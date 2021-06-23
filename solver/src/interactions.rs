4848211ec9cfef61040e9228e59538b0c2c61423
Nicholas Rodrigues Lordello <nicholas.lordello@gnosis.pm>
2021-06-23 20:35:36 +0200

    fmt

--- solver/src/interactions.rs
+++ solver/src/interactions.rs
@@ -3,7 +3,7 @@ mod erc20;
 mod uniswap;
 mod weth;
 
+pub use balancer::BalancerSwapGivenOutInteraction;
 pub use erc20::Erc20ApproveInteraction;
 pub use uniswap::UniswapInteraction;
 pub use weth::UnwrapWethInteraction;
-pub use balancer::BalancerSwapGivenOutInteraction;
