f63bdf4729760ad3b9820a339b5db307e8073dc3
josojo <josojo@hotmail.de>
2021-06-25 17:48:05 +0200

    adding the missing tests Felix asked for

--- solver/src/solver/matcha_solver.rs
+++ solver/src/solver/matcha_solver.rs
@@ -84,10 +84,10 @@ impl<F: AllowanceFetching> SingleOrderSolving for MatchaSolver<F> {
                 let swap = self.client.get_swap(query).await?;
                 tracing::debug!("proposed Matcha swap is {:?}", swap);
 
-                ensure!(
-                    swap.buy_amount >= order.buy_amount,
-                    "order limit price not respected",
-                );
+                if swap.buy_amount < order.buy_amount {
+                    tracing::debug!("Order limit price not respected");
+                    return Ok(None);
+                }
                 let settlement = Settlement::new(hashmap! {
                     order.sell_token => swap.sell_amount,
                     order.buy_token => swap.buy_amount,
@@ -113,10 +113,10 @@ impl<F: AllowanceFetching> SingleOrderSolving for MatchaSolver<F> {
                 let swap = self.client.get_swap(query).await?;
                 tracing::debug!("proposed Matcha swap is {:?}", swap);
 
-                ensure!(
-                    swap.sell_amount <= order.sell_amount,
-                    "order limit price not respected",
-                );
+                if swap.sell_amount > order.sell_amount {
+                    tracing::debug!("Order limit price not respected");
+                    return Ok(None);
+                }
                 let settlement = Settlement::new(hashmap! {
                     order.sell_token => swap.sell_amount,
                     order.buy_token => swap.buy_amount,
@@ -174,7 +174,7 @@ mod tests {
     use mockall::Sequence;
     use model::order::{Order, OrderCreation, OrderKind};
     use shared::dummy_contract;
-    use shared::transport::{create_env_test_transport, create_test_transport};
+    use shared::transport::{create_env_test_transport, create_test_transport, dummy};
 
     #[tokio::test]
     #[ignore]
@@ -209,7 +209,7 @@ mod tests {
     }
 
     #[tokio::test]
-    // #[ignore]
+    #[ignore]
     async fn solve_buy_order_on_matcha() {
         let web3 = Web3::new(create_env_test_transport());
         let chain_id = web3.eth().chain_id().await.unwrap().as_u64();
@@ -238,6 +238,135 @@ mod tests {
             .unwrap();
 
         println!("{:#?}", settlement);
+    }
+
+    #[tokio::test]
+    async fn test_satisfies_limit_price_for_sell_order() {
+        let mut client = Box::new(MockMatchaApi::new());
+        let mut allowance_fetcher = MockAllowanceFetching::new();
+
+        let sell_token = H160::from_low_u64_be(1);
+        let buy_token = H160::from_low_u64_be(1);
+
+        client.expect_get_swap().returning(|_| {
+            Ok(SwapResponse {
+            sell_amount: U256::from_dec_str("100").unwrap(),
+             buy_amount: U256::from_dec_str("91").unwrap(),
+             allowance_target: shared::addr!("def1c0ded9bec7f1a1670819833240f027b25eff"),
+            price: 0.91_f64,
+            to: shared::addr!("def1c0ded9bec7f1a1670819833240f027b25eff"),
+            data: web3::types::Bytes(hex::decode(
+                "d9627aa40000000000000000000000000000000000000000000000000000000000000080000000000000000000000000000000000000000000000000016345785d8a00000000000000000000000000000000000000000000000000001206e6c0056936e100000000000000000000000000000000000000000000000000000000000000010000000000000000000000000000000000000000000000000000000000000002000000000000000000000000c02aaa39b223fe8d0a0e5c4f27ead9083c756cc20000000000000000000000006810e776880c02933d47db1b9fc05908e5386b96869584cd0000000000000000000000001000000000000000000000000000000000000011000000000000000000000000000000000000000000000092415e982f60d431ba"
+            ).unwrap()),
+            value: U256::from_dec_str("0").unwrap(),
+        })});
+
+        allowance_fetcher
+            .expect_existing_allowance()
+            .returning(|_, _| Ok(U256::zero()));
+
+        let solver = MatchaSolver {
+            settlement_contract: GPv2Settlement::at(&dummy::web3(), H160::zero()),
+            client,
+            allowance_fetcher,
+        };
+
+        let order_passing_limit = LimitOrder {
+            sell_token,
+            buy_token,
+            sell_amount: 100.into(),
+            buy_amount: 90.into(),
+            kind: model::order::OrderKind::Sell,
+            ..Default::default()
+        };
+        let order_violating_limit = LimitOrder {
+            sell_token,
+            buy_token,
+            sell_amount: 100.into(),
+            buy_amount: 110.into(),
+            kind: model::order::OrderKind::Sell,
+            ..Default::default()
+        };
+
+        let result = solver
+            .settle_order(order_passing_limit)
+            .await
+            .unwrap()
+            .unwrap();
+        assert_eq!(
+            result.clearing_prices(),
+            &hashmap! {
+                sell_token => 99.into(),
+                buy_token => 91.into(),
+            }
+        );
+
+        let result = solver.settle_order(order_violating_limit).await.unwrap();
+        assert!(result.is_none());
+    }
+    #[tokio::test]
+    async fn test_satisfies_limit_price_for_buy_order() {
+        let mut client = Box::new(MockMatchaApi::new());
+        let mut allowance_fetcher = MockAllowanceFetching::new();
+
+        let sell_token = H160::from_low_u64_be(1);
+        let buy_token = H160::from_low_u64_be(1);
+
+        client.expect_get_swap().returning(|_| {
+            Ok(SwapResponse {
+            sell_amount: U256::from_dec_str("100").unwrap(),
+             buy_amount: U256::from_dec_str("91").unwrap(),
+             allowance_target: shared::addr!("def1c0ded9bec7f1a1670819833240f027b25eff"),
+            price: 0.91_f64,
+            to: shared::addr!("def1c0ded9bec7f1a1670819833240f027b25eff"),
+            data: web3::types::Bytes(hex::decode(
+                "d9627aa40000000000000000000000000000000000000000000000000000000000000080000000000000000000000000000000000000000000000000016345785d8a00000000000000000000000000000000000000000000000000001206e6c0056936e100000000000000000000000000000000000000000000000000000000000000010000000000000000000000000000000000000000000000000000000000000002000000000000000000000000c02aaa39b223fe8d0a0e5c4f27ead9083c756cc20000000000000000000000006810e776880c02933d47db1b9fc05908e5386b96869584cd0000000000000000000000001000000000000000000000000000000000000011000000000000000000000000000000000000000000000092415e982f60d431ba"
+            ).unwrap()),
+            value: U256::from_dec_str("0").unwrap(),
+        })});
+
+        allowance_fetcher
+            .expect_existing_allowance()
+            .returning(|_, _| Ok(U256::zero()));
+
+        let solver = MatchaSolver {
+            settlement_contract: GPv2Settlement::at(&dummy::web3(), H160::zero()),
+            client,
+            allowance_fetcher,
+        };
+
+        let order_passing_limit = LimitOrder {
+            sell_token,
+            buy_token,
+            sell_amount: 101.into(),
+            buy_amount: 91.into(),
+            kind: model::order::OrderKind::Buy,
+            ..Default::default()
+        };
+        let order_violating_limit = LimitOrder {
+            sell_token,
+            buy_token,
+            sell_amount: 99.into(),
+            buy_amount: 91.into(),
+            kind: model::order::OrderKind::Buy,
+            ..Default::default()
+        };
+
+        let result = solver
+            .settle_order(order_passing_limit)
+            .await
+            .unwrap()
+            .unwrap();
+        assert_eq!(
+            result.clearing_prices(),
+            &hashmap! {
+                sell_token => 99.into(),
+                buy_token => 91.into(),
+            }
+        );
+
+        let result = solver.settle_order(order_violating_limit).await.unwrap();
+        assert!(result.is_none());
     }
 
     #[tokio::test]
