0eb0cda8b363826fa79a42d76d756443ff34e19e
Valentin <77051586+vkgnosis@users.noreply.github.com>
2021-07-14 13:22:38 +0200

    Make bad token detector more generic over amm pool sources
    Currently we use AmmPairProvider which is only implemented for Sushiswap
    and UniswapV2. We introduce a trait to be more generic and can in the
    future be implemented for more sources.

--- orderbook/src/main.rs
+++ orderbook/src/main.rs
@@ -18,7 +18,7 @@ use shared::{
     bad_token::{
         cache::CachingDetector,
         list_based::{ListBasedDetector, UnknownTokenStrategy},
-        trace_call::TraceCallDetector,
+        trace_call::{AmmPairProviderFinder, TokenOwnerFinding, TraceCallDetector},
     },
     current_block::current_block_stream,
     maintenance::ServiceMaintenance,
@@ -198,9 +198,18 @@ async fn main() {
     allowed_tokens.push(BUY_ETH_ADDRESS);
     let unsupported_tokens = args.unsupported_tokens;
 
+    let finders = pair_providers
+        .iter()
+        .map(|provider| -> Arc<dyn TokenOwnerFinding> {
+            Arc::new(AmmPairProviderFinder {
+                inner: provider.clone(),
+                base_tokens: base_tokens.iter().copied().collect(),
+            })
+        })
+        .collect();
     let trace_call_detector = TraceCallDetector {
         web3: web3.clone(),
-        pools: pair_providers.clone(),
+        finders,
         base_tokens: base_tokens.clone(),
         settlement_contract: settlement_contract.address(),
     };
