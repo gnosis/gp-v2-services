a6c48fad7aea6a7a52b486aa222a1ccf9badb2f1
Nicholas Rodrigues Lordello <nicholas.lordello@gnosis.pm>
2021-08-02 10:06:48 +0200

    Update order flag encoding

--- solver/src/encoding.rs
+++ solver/src/encoding.rs
@@ -1,6 +1,6 @@
 use ethcontract::Bytes;
 use model::{
-    order::{OrderCreation, OrderKind},
+    order::{BalanceFrom, BalanceTo, OrderCreation, OrderKind},
     SigningScheme,
 };
 use primitive_types::{H160, U256};
@@ -44,16 +44,23 @@ pub fn encode_trade(
 fn order_flags(order: &OrderCreation) -> U256 {
     let mut result = 0u8;
     result |= match order.kind {
-        OrderKind::Sell => 0,
+        OrderKind::Sell => 0b0,
         OrderKind::Buy => 0b1,
     };
-    if order.partially_fillable {
-        result |= 0b10;
-    };
+    result |= (order.partially_fillable as u8) << 1;
+    result |= match order.sell_token_balance {
+        BalanceFrom::Erc20 => 0b00,
+        BalanceFrom::External => 0b10,
+        BalanceFrom::Internal => 0b11,
+    } << 2;
+    result |= match order.buy_token_balance {
+        BalanceTo::Erc20 => 0b0,
+        BalanceTo::Internal => 0b1,
+    } << 4;
     result |= match order.signing_scheme {
-        SigningScheme::Eip712 => 0,
-        SigningScheme::EthSign => 0b100,
-    };
+        SigningScheme::Eip712 => 0b00,
+        SigningScheme::EthSign => 0b01,
+    } << 5;
     result.into()
 }
 
@@ -69,4 +76,72 @@ pub struct EncodedSettlement {
     pub clearing_prices: Vec<U256>,
     pub trades: Vec<EncodedTrade>,
     pub interactions: [Vec<EncodedInteraction>; 3],
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn order_flag_permutations() {
+        for (order, flags) in &[
+            (
+                OrderCreation {
+                    kind: OrderKind::Sell,
+                    partially_fillable: false,
+                    sell_token_balance: BalanceFrom::Erc20,
+                    buy_token_balance: BalanceTo::Erc20,
+                    signing_scheme: SigningScheme::Eip712,
+                    ..Default::default()
+                },
+                0b0000000,
+            ),
+            (
+                OrderCreation {
+                    kind: OrderKind::Sell,
+                    partially_fillable: true,
+                    sell_token_balance: BalanceFrom::Erc20,
+                    buy_token_balance: BalanceTo::Internal,
+                    signing_scheme: SigningScheme::Eip712,
+                    ..Default::default()
+                },
+                0b0010010,
+            ),
+            (
+                OrderCreation {
+                    kind: OrderKind::Buy,
+                    partially_fillable: false,
+                    sell_token_balance: BalanceFrom::External,
+                    buy_token_balance: BalanceTo::Erc20,
+                    signing_scheme: SigningScheme::Eip712,
+                    ..Default::default()
+                },
+                0b0001001,
+            ),
+            (
+                OrderCreation {
+                    kind: OrderKind::Sell,
+                    partially_fillable: false,
+                    sell_token_balance: BalanceFrom::Internal,
+                    buy_token_balance: BalanceTo::Erc20,
+                    signing_scheme: SigningScheme::EthSign,
+                    ..Default::default()
+                },
+                0b0101100,
+            ),
+            (
+                OrderCreation {
+                    kind: OrderKind::Buy,
+                    partially_fillable: true,
+                    sell_token_balance: BalanceFrom::Internal,
+                    buy_token_balance: BalanceTo::Internal,
+                    signing_scheme: SigningScheme::EthSign,
+                    ..Default::default()
+                },
+                0b0111111,
+            ),
+        ] {
+            assert_eq!(order_flags(order), U256::from(*flags));
+        }
+    }
 }
