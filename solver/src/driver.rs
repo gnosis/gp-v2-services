gnosis/gp-v2-services

commit d917e084f339768ece257cf1c10187c2325e7e46

Add config option for solver accounts (#1010)
Fix #827.

### Test Plan
Manual testing

solver/src/settlement_submission/archer_settlement.rs
@@ -339,9 +339,7 @@ mod tests {
         assert_eq!(chain_id, 1);
         let private_key: PrivateKey = std::env::var("PRIVATE_KEY").unwrap().parse().unwrap();
         let account = Account::Offline(private_key, Some(chain_id));
-        let contract = crate::get_settlement_contract(&web3, account.clone())
-            .await
-            .unwrap();
+        let contract = crate::get_settlement_contract(&web3).await.unwrap();
         let archer_api = ArcherApi::new(
             std::env::var("ARCHER_AUTHORIZATION").unwrap(),
             Client::new(),

e2e/tests/eth_integration.rs
@@ -53,7 +53,7 @@ async fn eth_integration(web3: Web3) {
     let trader_buy_eth_b =
         Account::Offline(PrivateKey::from_raw(TRADER_BUY_ETH_B_PK).unwrap(), None);
 
-    let gpv2 = GPv2::fetch(&web3, &solver_account).await;
+    let gpv2 = GPv2::fetch(&web3).await;
     let UniswapContracts {
         uniswap_factory,
         uniswap_router,

e2e/tests/onchain_settlement.rs
@@ -53,7 +53,7 @@ async fn onchain_settlement(web3: Web3) {
     let trader_a = Account::Offline(PrivateKey::from_raw(TRADER_A_PK).unwrap(), None);
     let trader_b = Account::Offline(PrivateKey::from_raw(TRADER_B_PK).unwrap(), None);
 
-    let gpv2 = GPv2::fetch(&web3, &solver_account).await;
+    let gpv2 = GPv2::fetch(&web3).await;
     let UniswapContracts {
         uniswap_factory,
         uniswap_router,

e2e/tests/services.rs
@@ -1,10 +1,7 @@
 use contracts::{
     BalancerV2Vault, ERC20Mintable, GPv2Settlement, UniswapV2Factory, UniswapV2Router02, WETH9,
 };
-use ethcontract::{
-    prelude::{Account, U256},
-    H160,
-};
+use ethcontract::{prelude::U256, H160};
 use maplit::hashset;
 use model::DomainSeparator;
 use orderbook::{
@@ -69,11 +66,11 @@ pub struct GPv2 {
 }
 
 impl GPv2 {
-    pub async fn fetch(web3: &Web3, designated_solver: &Account) -> Self {
+    pub async fn fetch(web3: &Web3) -> Self {
         let vault = BalancerV2Vault::deployed(web3)
             .await
             .expect("Failed to load deployed BalancerV2Vault");
-        let settlement = solver::get_settlement_contract(web3, designated_solver.clone())
+        let settlement = solver::get_settlement_contract(web3)
             .await
             .expect("Failed to load deployed GPv2Settlement");
         let allowance = settlement

e2e/tests/settlement_without_onchain_liquidity.rs
@@ -51,7 +51,7 @@ async fn onchain_settlement_without_liquidity(web3: Web3) {
     let solver_account = Account::Local(accounts[0], None);
     let trader_account = Account::Offline(PrivateKey::from_raw(TRADER_A_PK).unwrap(), None);
 
-    let gpv2 = GPv2::fetch(&web3, &solver_account).await;
+    let gpv2 = GPv2::fetch(&web3).await;
     let UniswapContracts {
         uniswap_factory,
         uniswap_router,

e2e/tests/vault_balances.rs
@@ -47,7 +47,7 @@ async fn vault_balances(web3: Web3) {
     let solver_account = Account::Local(accounts[0], None);
     let trader = Account::Offline(PrivateKey::from_raw(TRADER).unwrap(), None);
 
-    let gpv2 = GPv2::fetch(&web3, &solver_account).await;
+    let gpv2 = GPv2::fetch(&web3).await;
     let UniswapContracts {
         uniswap_factory,
         uniswap_router,

solver/src/lib.rs
@@ -14,17 +14,8 @@ pub mod solver;
 mod test;
 
 use anyhow::Result;
-use ethcontract::{contract::MethodDefaults, Account};
 use shared::Web3;
 
-pub async fn get_settlement_contract(
-    web3: &Web3,
-    account: Account,
-) -> Result<contracts::GPv2Settlement> {
-    let mut settlement_contract = contracts::GPv2Settlement::deployed(web3).await?;
-    *settlement_contract.defaults_mut() = MethodDefaults {
-        from: Some(account),
-        ..Default::default()
-    };
-    Ok(settlement_contract)
+pub async fn get_settlement_contract(web3: &Web3) -> Result<contracts::GPv2Settlement> {
+    Ok(contracts::GPv2Settlement::deployed(web3).await?)
 }

solver/src/main.rs
@@ -59,7 +59,7 @@ struct Arguments {
 
     /// The private key used by the driver to sign transactions.
     #[structopt(short = "k", long, env = "PRIVATE_KEY", hide_env_values = true)]
-    private_key: PrivateKey,
+    private_key: Option<PrivateKey>,
 
     /// The target confirmation time for settlement transactions used to estimate gas price.
     #[structopt(
@@ -89,6 +89,16 @@ struct Arguments {
         use_delimiter = true,
     )]
     solvers: Vec<SolverType>,
+
+    /// Individual private keys for each solver
+    #[structopt(
+        long,
+        env = "SOLVER_PRIVATE_KEYS",
+        case_insensitive = true,
+        use_delimiter = true,
+        hide_env_values = true
+    )]
+    solver_private_keys: Option<Vec<PrivateKey>>,
 
     /// A settlement must contain at least one order older than this duration for it to be applied.
     /// Larger values delay individual settlements more but have a higher coincidence of wants
@@ -222,8 +232,7 @@ async fn main() {
         .await
         .expect("failed to get network id");
     let network_name = network_name(&network_id, chain_id);
-    let account = Account::Offline(args.private_key, Some(chain_id));
-    let settlement_contract = solver::get_settlement_contract(&web3, account.clone())
+    let settlement_contract = solver::get_settlement_contract(&web3)
         .await
         .expect("couldn't load deployed settlement");
     let native_token_contract = WETH9::deployed(&web3)
@@ -337,10 +346,31 @@ async fn main() {
         web3.clone(),
     )
     .await;
+
+    let solvers = {
+        if let Some(private_keys) = args.solver_private_keys {
+            assert!(
+                private_keys.len() == args.solvers.len(),
+                "number of solver does not match the number of private keys"
+            );
+
+            private_keys
+                .into_iter()
+                .map(|private_key| Account::Offline(private_key, Some(chain_id)))
+                .zip(args.solvers)
+                .collect()
+        } else if let Some(private_key) = args.private_key {
+            std::iter::repeat(Account::Offline(private_key, Some(chain_id)))
+                .zip(args.solvers)
+                .collect()
+        } else {
+            panic!("either SOLVER_PRIVATE_KEY or PRIVATE_KEY must be set")
+        }
+    };
+
     let solver = solver::solver::create(
-        account.clone(),
         web3.clone(),
-        args.solvers,
+        solvers,
         base_tokens,
         native_token_contract.address(),
         args.mip_solver_url,

commit 4d9cead51f9c7727f1015308a3d06f8dd45349e1

Fix gas estimation (#1022)
This fixes a bug introduced in #1010: we didn't set the `from` account for calls to `estimate_gas`.

### Test Plan
Given that CI didn't catch the bug in #1010, I'm highly skeptical of it. My local cowswap installation is broken at the moment, perhaps someone else could test it? Or we could deploy to prestable and check it there.

solver/src/driver.rs
@@ -333,6 +333,7 @@ impl Driver {
                 let gas_estimate = settlement_submission::estimate_gas(
                     &self.settlement_contract,
                     &settlement.clone().into(),
+                    solver.account().clone(),
                 )
                 .await
                 .ok()?;

solver/src/settlement_simulation.rs
@@ -61,9 +61,9 @@ pub async fn simulate_settlements(
             let method = crate::settlement_submission::retry::settle_method_builder(
                 contract,
                 settlement.clone().into(),
+                solver.account().clone(),
             )
-            .gas_price(gas_price)
-            .from(solver.account().clone());
+            .gas_price(gas_price);
             let transaction_builder = method.tx.clone();
             let view = method
                 .view()

solver/src/settlement_submission.rs
@@ -25,8 +25,9 @@ const GAS_PRICE_REFRESH_INTERVAL: Duration = Duration::from_secs(15);
 pub async fn estimate_gas(
     contract: &GPv2Settlement,
     settlement: &EncodedSettlement,
+    from: Account,
 ) -> Result<U256, ExecutionError> {
-    retry::settle_method_builder(contract, settlement.clone())
+    retry::settle_method_builder(contract, settlement.clone(), from)
         .tx
         .estimate_gas()
         .await

solver/src/settlement_submission/archer_settlement.rs
@@ -211,13 +211,16 @@ impl<'a> ArcherSolutionSubmitter<'a> {
                 .append_to_execution_plan(block_coinbase::PayBlockCoinbase {
                     amount: tx_gas_cost_in_ether_wei,
                 });
-            let method = super::retry::settle_method_builder(self.contract, settlement.into())
-                .from(self.account.clone())
-                .nonce(nonce)
-                // Wouldn't work because the function isn't payable.
-                // .value(tx_gas_cost_in_ether_wei)
-                .gas(U256::from_f64_lossy(gas_limit))
-                .gas_price(GasPrice::Value(U256::zero()));
+            let method = super::retry::settle_method_builder(
+                self.contract,
+                settlement.into(),
+                self.account.clone(),
+            )
+            .nonce(nonce)
+            // Wouldn't work because the function isn't payable.
+            // .value(tx_gas_cost_in_ether_wei)
+            .gas(U256::from_f64_lossy(gas_limit))
+            .gas_price(GasPrice::Value(U256::zero()));
 
             // simulate transaction
 
@@ -349,10 +352,13 @@ mod tests {
         let gas_price_cap = 100e9;
 
         let settlement = Settlement::new(Default::default());
-        let gas_estimate =
-            crate::settlement_submission::estimate_gas(&contract, &settlement.clone().into())
-                .await
-                .unwrap();
+        let gas_estimate = crate::settlement_submission::estimate_gas(
+            &contract,
+            &settlement.clone().into(),
+            account.clone(),
+        )
+        .await
+        .unwrap();
 
         let submitter = ArcherSolutionSubmitter {
             web3: &web3,

solver/src/settlement_submission/retry.rs
@@ -67,11 +67,11 @@ impl<'a> TransactionSending for SettlementSender<'a> {
 
     async fn send(&self, gas_price: f64) -> Self::Output {
         tracing::info!("submitting solution transaction at gas price {}", gas_price);
-        let mut method = settle_method_builder(self.contract, self.settlement.clone())
-            .nonce(self.nonce)
-            .gas_price(GasPrice::Value(U256::from_f64_lossy(gas_price)))
-            .gas(U256::from_f64_lossy(self.gas_limit))
-            .from(self.account.clone());
+        let mut method =
+            settle_method_builder(self.contract, self.settlement.clone(), self.account.clone())
+                .nonce(self.nonce)
+                .gas_price(GasPrice::Value(U256::from_f64_lossy(gas_price)))
+                .gas(U256::from_f64_lossy(self.gas_limit));
         method.tx.resolve = Some(ResolveCondition::Confirmed(ConfirmParams::mined()));
         let result = method.send().await.map(|tx| tx.hash());
         SettleResult(result)
@@ -81,13 +81,16 @@ impl<'a> TransactionSending for SettlementSender<'a> {
 pub fn settle_method_builder(
     contract: &GPv2Settlement,
     settlement: EncodedSettlement,
+    from: Account,
 ) -> DynMethodBuilder<()> {
-    contract.settle(
-        settlement.tokens,
-        settlement.clearing_prices,
-        settlement.trades,
-        settlement.interactions,
-    )
+    contract
+        .settle(
+            settlement.tokens,
+            settlement.clearing_prices,
+            settlement.trades,
+            settlement.interactions,
+        )
+        .from(from)
 }
 
 // We never send cancellations but we still need to have types that implement the traits.