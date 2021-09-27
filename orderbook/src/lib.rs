4bc47c035cfe04d640ca3f405b03b83b8f63b28a
Tamika Nomara <tamika.nomara@gnosis.pm>
2021-09-27 14:37:55 +0300

    API metrics

--- orderbook/src/lib.rs
+++ orderbook/src/lib.rs
@@ -13,7 +13,6 @@ use anyhow::{anyhow, Context as _, Result};
 use contracts::GPv2Settlement;
 use database::trades::TradeRetrieving;
 use fee::EthAwareMinFeeCalculator;
-use metrics::Metrics;
 use model::DomainSeparator;
 use shared::{
     metrics::{serve_metrics, DEFAULT_METRICS_PORT},
@@ -28,14 +27,12 @@ pub fn serve_task(
     fee_calculator: Arc<EthAwareMinFeeCalculator>,
     price_estimator: Arc<dyn PriceEstimating>,
     address: SocketAddr,
-    metrics: Arc<Metrics>,
 ) -> JoinHandle<()> {
     let filter = api::handle_all_routes(
         database,
         orderbook.clone(),
         fee_calculator,
         price_estimator,
-        metrics,
     );
     let mut metrics_address = address;
     tracing::info!(%address, "serving order book");
