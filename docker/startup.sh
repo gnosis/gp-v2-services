061c823a13bb2cffb17a9f0887a75e31f89907e8
Felix Leupold <felixleupold90@gmail.com>
2021-09-29 13:36:57 +0200

    Expose main process to interrupt signals

--- docker/startup.sh
+++ docker/startup.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 # The purpose of this script is to set a log regex to forward error logs to stderr and other logs to
 # stdout. This allows us to easily configure alerts when any message goes to stderr.
-"$@" | regex-stream-split "^\d+-\d+-\d+T\d+:\d+:\d+\.\d+Z\s+(TRACE|DEBUG|INFO|WARN)" "^\d+-\d+-\d+T\d+:\d+:\d+\.\d+Z\s+ERROR"
+
+# The main command is run in the background so that it receives interrupt signals
+# The stream-split command deliberately ignores interrupts (otherwise we may lose logs on teardown). It terminates automatically
+# once the main command (stdin) terminates.
+("$@" &) | (trap '' INT; regex-stream-split '^\d+-\d+-\d+T\d+:\d+:\d+\.\d+Z\s+(TRACE|DEBUG|INFO|WARN)' '^\d+-\d+-\d+T\d+:\d+:\d+\.\d+Z\s+ERROR')
+
