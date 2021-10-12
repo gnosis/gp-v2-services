7e83cbac5ee51f3546d77ad8ae9601043939efc8
Ben Smith <bh2smith@gmail.com>
2021-10-12 09:37:35 +0200

    add new batch metrics for matched order classification counts

--- solver/src/metrics.rs
+++ solver/src/metrics.rs
@@ -29,6 +29,8 @@ pub trait SolverMetrics {
     fn settlement_simulation_failed_on_latest(&self, solver: &'static str);
     fn settlement_simulation_failed(&self, solver: &'static str);
     fn settlement_submitted(&self, successful: bool, solver: &'static str);
+    fn orders_matched_but_cancelled(&self, count: usize);
+    fn orders_matched_but_liquidity(&self, count: usize);
     fn orders_matched_but_not_settled(&self, count: usize);
     fn runloop_completed(&self);
 }
@@ -41,6 +43,8 @@ pub struct Metrics {
     liquidity: IntGaugeVec,
     settlement_simulations: IntCounterVec,
     settlement_submissions: IntCounterVec,
+    matched_but_cancelled: IntCounter,
+    matched_but_liquidity: IntCounter,
     matched_but_unsettled_orders: IntCounter,
     transport_requests: HistogramVec,
     pool_cache_hits: IntCounter,
@@ -94,6 +98,18 @@ impl Metrics {
         )?;
         registry.register(Box::new(settlement_submissions.clone()))?;
 
+        let matched_but_cancelled = IntCounter::new(
+            "orders_matched_cancelled",
+            "Counter for the number of orders for which at least one solver computed an execution which was already cancelled",
+        )?;
+        registry.register(Box::new(matched_but_cancelled.clone()))?;
+
+        let matched_but_liquidity = IntCounter::new(
+            "orders_matched_liquidity",
+            "Counter for the number of orders for which at least one solver computed an execution which was from a known liquidity provider",
+        )?;
+        registry.register(Box::new(matched_but_liquidity.clone()))?;
+
         let matched_but_unsettled_orders = IntCounter::new(
             "orders_matched_not_settled",
             "Counter for the number of orders for which at least one solver computed an execution which was not chosen in this run-loop",
@@ -126,6 +142,8 @@ impl Metrics {
             liquidity,
             settlement_simulations,
             settlement_submissions,
+            matched_but_cancelled,
+            matched_but_liquidity,
             matched_but_unsettled_orders,
             transport_requests,
             pool_cache_hits,
@@ -202,6 +220,14 @@ impl SolverMetrics for Metrics {
             .inc()
     }
 
+    fn orders_matched_but_cancelled(&self, count: usize) {
+        self.matched_but_cancelled.inc_by(count as u64);
+    }
+
+    fn orders_matched_but_liquidity(&self, count: usize) {
+        self.matched_but_liquidity.inc_by(count as u64);
+    }
+
     fn orders_matched_but_not_settled(&self, count: usize) {
         self.matched_but_unsettled_orders.inc_by(count as u64);
     }
@@ -262,6 +288,8 @@ impl SolverMetrics for NoopMetrics {
     fn settlement_simulation_failed_on_latest(&self, _: &'static str) {}
     fn settlement_simulation_failed(&self, _: &'static str) {}
     fn settlement_submitted(&self, _: bool, _: &'static str) {}
+    fn orders_matched_but_cancelled(&self, _: usize) {}
+    fn orders_matched_but_liquidity(&self, _: usize) {}
     fn orders_matched_but_not_settled(&self, _: usize) {}
     fn runloop_completed(&self) {}
 }
