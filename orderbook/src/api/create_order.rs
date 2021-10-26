0cf763a1d2c48c7085794adc611b6e8f94937ad5
Ben Smith <bh2smith@gmail.com>
2021-10-26 20:26:18 +0200

    revert all changes to api/create_order.rs

--- orderbook/src/api/create_order.rs
+++ orderbook/src/api/create_order.rs
@@ -1,15 +1,11 @@
 use crate::{
-    api::{extract_payload, IntoWarpReply},
+    api::{extract_payload, WarpReplyConverting},
     orderbook::{AddOrderResult, Orderbook},
 };
 use anyhow::Result;
 use model::order::OrderCreationPayload;
 use std::{convert::Infallible, sync::Arc};
-use warp::{
-    hyper::StatusCode,
-    reply::{with_status, Json, WithStatus},
-    Filter, Rejection, Reply,
-};
+use warp::{hyper::StatusCode, Filter, Rejection, Reply};
 
 pub fn create_order_request(
 ) -> impl Filter<Extract = (OrderCreationPayload,), Error = Rejection> + Clone {
@@ -18,21 +14,21 @@ pub fn create_order_request(
         .and(extract_payload())
 }
 
-impl IntoWarpReply for AddOrderResult {
-    fn into_warp_reply(self) -> WithStatus<Json> {
-        match self {
-            AddOrderResult::Added(uid) => with_status(warp::reply::json(&uid), StatusCode::CREATED),
-            AddOrderResult::OrderValidation(err) => err.into_warp_reply(),
-            AddOrderResult::UnsupportedSignature => with_status(
-                super::error("UnsupportedSignature", "signing scheme is not supported"),
-                StatusCode::BAD_REQUEST,
-            ),
-            AddOrderResult::DuplicatedOrder => with_status(
-                super::error("DuplicatedOrder", "order already exists"),
-                StatusCode::BAD_REQUEST,
-            ),
-        }
-    }
+pub fn create_order_response(result: Result<AddOrderResult>) -> impl Reply {
+    let (body, status_code) = match result {
+        Ok(AddOrderResult::Added(uid)) => (warp::reply::json(&uid), StatusCode::CREATED),
+        Ok(AddOrderResult::OrderValidation(err)) => err.into_warp_reply(),
+        Ok(AddOrderResult::UnsupportedSignature) => (
+            super::error("UnsupportedSignature", "signing scheme is not supported"),
+            StatusCode::BAD_REQUEST,
+        ),
+        Ok(AddOrderResult::DuplicatedOrder) => (
+            super::error("DuplicatedOrder", "order already exists"),
+            StatusCode::BAD_REQUEST,
+        ),
+        Err(_) => (super::internal_error(), StatusCode::INTERNAL_SERVER_ERROR),
+    };
+    warp::reply::with_status(body, status_code)
 }
 
 pub fn create_order(
@@ -46,10 +42,7 @@ pub fn create_order(
             if let Err(err) = &result {
                 tracing::error!(?err, ?order_payload_clone, "add_order error");
             }
-            Result::<_, Infallible>::Ok(match result {
-                Ok(result) => result.into_warp_reply(),
-                Err(_) => with_status(super::internal_error(), StatusCode::INTERNAL_SERVER_ERROR),
-            })
+            Result::<_, Infallible>::Ok(create_order_response(result))
         }
     })
 }
@@ -78,7 +71,7 @@ mod tests {
     #[tokio::test]
     async fn create_order_response_created() {
         let uid = OrderUid([1u8; 56]);
-        let response = AddOrderResult::Added(uid).into_warp_reply().into_response();
+        let response = create_order_response(Ok(AddOrderResult::Added(uid))).into_response();
         assert_eq!(response.status(), StatusCode::CREATED);
         let body = response_body(response).await;
         let body: serde_json::Value = serde_json::from_slice(body.as_slice()).unwrap();
@@ -90,9 +83,7 @@ mod tests {
 
     #[tokio::test]
     async fn create_order_response_duplicate() {
-        let response = AddOrderResult::DuplicatedOrder
-            .into_warp_reply()
-            .into_response();
+        let response = create_order_response(Ok(AddOrderResult::DuplicatedOrder)).into_response();
         assert_eq!(response.status(), StatusCode::BAD_REQUEST);
         let body = response_body(response).await;
         let body: serde_json::Value = serde_json::from_slice(body.as_slice()).unwrap();
