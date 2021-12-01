0120bf6d4c2cca2852f59c39c3b03870d0303ced
Nicholas Rodrigues Lordello <nlordell@gmail.com>
2021-12-01 08:46:06 +0000

    Rename manual test and improve unit test to ensure correct kind of deployment information

--- crates/shared/src/sources/balancer_v2/event_handler.rs
+++ crates/shared/src/sources/balancer_v2/event_handler.rs
@@ -340,7 +340,7 @@ mod tests {
 
     #[tokio::test]
     #[ignore]
-    async fn balancer_index_pool_events() {
+    async fn balancer_indexed_pool_events_match_subgraph() {
         let transport = transport::create_env_test_transport();
         let web3 = Web3::new(transport);
         let chain_id = web3.eth().chain_id().await.unwrap().as_u64();
