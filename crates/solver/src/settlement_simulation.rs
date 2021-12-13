99d4a0e7e18a22772b5a5df044d4f3d2c6bed357
josojo <josojo@hotmail.de>
2021-12-13 13:46:00 +0100

    updating the comments only

--- crates/solver/src/settlement_simulation.rs
+++ crates/solver/src/settlement_simulation.rs
@@ -96,7 +96,7 @@ pub async fn simulate_and_error_with_tenderly_link(
                 // Since we now supply the gas price for the simulation, make sure to also
                 // set a gas limit so we don't get failed simulations because of insufficient
                 // solver balance for the default ~15M gas limit. Limit to around the
-                // block gas limit / 2 (since just filling the halve a block is unrealistic).
+                // block gas limit / 2 (as settling bigger tx is unrealistic anyways).
                 .gas(15_000_000.into());
             (view.batch_call(&mut batch), transaction_builder)
         })
