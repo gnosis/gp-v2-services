9b2d86d168609219841f0b7abfb60907570c8859
Martin Beckmann <martin.beckmann@gnosis.pm>
2022-01-03 11:24:29 +0100

    Implement unit tests
    Tests 4 scenarios
    1) dropping the unwrap succeeds
    2) convert way more WETH than the settlment needs to pay out
    3) optimizations don't apply because the buffer is too small
    4) optimizations don't apply because an error occurred

--- crates/solver/src/settlement_post_processing/optimize_unwrapping.rs
+++ crates/solver/src/settlement_post_processing/optimize_unwrapping.rs
@@ -57,3 +57,145 @@ where
 
     Ok(())
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::interactions::UnwrapWethInteraction;
+    use shared::dummy_contract;
+    use std::collections::HashMap;
+    use std::sync::{Arc, Mutex};
+
+    fn to_wei(base: u128) -> U256 {
+        U256::from(base) * U256::from(10).pow(18.into())
+    }
+
+    fn settlement_with_unwrap(weth: &WETH9, amount: U256) -> Settlement {
+        let mut settlement = Settlement::with_trades(HashMap::default(), Vec::default());
+        if !amount.is_zero() {
+            settlement.encoder.add_unwrap(UnwrapWethInteraction {
+                weth: weth.clone(),
+                amount,
+            });
+        }
+        assert_eq!(amount, settlement.encoder.amount_to_unwrap(weth.address()));
+        settlement
+    }
+
+    #[tokio::test]
+    async fn drop_unwrap_if_eth_buffer_is_big_enough() {
+        let first_optimization_succeeds = |_: Settlement| async { true };
+        let get_weth_balance_succeeds = || async { Ok(U256::zero()) };
+        let weth = dummy_contract!(WETH9, [0x42; 20]);
+
+        let mut settlement = settlement_with_unwrap(&weth, to_wei(1));
+
+        optimize_unwrapping(
+            &mut settlement,
+            &first_optimization_succeeds,
+            &get_weth_balance_succeeds,
+            &weth,
+            0.6,
+        )
+        .await
+        .unwrap();
+
+        // no unwraps left because we pay 1 ETH from our buffer
+        assert_eq!(
+            U256::zero(),
+            settlement.encoder.amount_to_unwrap(weth.address())
+        );
+    }
+
+    #[tokio::test]
+    async fn bulk_convert_if_weth_buffer_is_big_enough() {
+        let successes = Arc::new(Mutex::new(vec![true, false]));
+        let second_optimization_succeeds =
+            |_: Settlement| async { successes.lock().unwrap().pop().unwrap() };
+        let get_weth_balance_succeeds = || async { Ok(to_wei(100)) };
+        let weth = dummy_contract!(WETH9, [0x42; 20]);
+
+        let mut settlement = settlement_with_unwrap(&weth, to_wei(10));
+        let unwrap_factor = 0.6;
+
+        optimize_unwrapping(
+            &mut settlement,
+            &second_optimization_succeeds,
+            &get_weth_balance_succeeds,
+            &weth,
+            unwrap_factor,
+        )
+        .await
+        .unwrap();
+
+        // we unwrap way more than needed to hopefully drop unwraps on the next few settlements
+        assert_eq!(
+            to_wei(60),
+            settlement.encoder.amount_to_unwrap(weth.address())
+        );
+    }
+
+    #[tokio::test]
+    async fn leave_settlement_unchanged_if_buffers_are_too_small_for_optimizations() {
+        // Although we would have enough WETH to cover the ETH payout, we pretend the bulk unwrap
+        // would fail anyway. This can happen if the execution_plan of the settlement also tries to
+        // use the WETH buffer (In this case more than 10 WETH).
+        let no_optimization_succeeds = |_: Settlement| async { false };
+        let get_weth_balance_succeeds = || async { Ok(to_wei(100)) };
+        let weth = dummy_contract!(WETH9, [0x42; 20]);
+
+        let eth_to_unwrap = to_wei(50);
+        let mut settlement = settlement_with_unwrap(&weth, eth_to_unwrap);
+
+        optimize_unwrapping(
+            &mut settlement,
+            &no_optimization_succeeds,
+            &get_weth_balance_succeeds,
+            &weth,
+            0.6,
+        )
+        .await
+        .unwrap();
+
+        // the settlement has been left unchanged
+        assert_eq!(
+            eth_to_unwrap,
+            settlement.encoder.amount_to_unwrap(weth.address())
+        );
+    }
+
+    #[tokio::test]
+    async fn errors_get_propagated_and_leave_settlement_unchanged() {
+        //second optimization would work if the algorithm would get so far
+        let successes = Arc::new(Mutex::new(vec![true, false]));
+        let second_optimization_succeeds =
+            |_: Settlement| async { successes.lock().unwrap().pop().unwrap() };
+
+        let error_message = "can't determine WETH balance";
+        let get_weth_balance_fails = || async { Err(anyhow::anyhow!(error_message)) };
+        let weth = dummy_contract!(WETH9, [0x42; 20]);
+
+        let eth_to_unwrap = to_wei(60);
+        let mut settlement = settlement_with_unwrap(&weth, eth_to_unwrap);
+
+        let optimization_result = optimize_unwrapping(
+            &mut settlement,
+            &second_optimization_succeeds,
+            &get_weth_balance_fails,
+            &weth,
+            0.6,
+        )
+        .await;
+
+        assert_eq!(
+            error_message,
+            optimization_result.as_ref().unwrap_err().to_string(),
+        );
+
+        // settlement had been left unchanged because we encountered an error while optimizing it
+        assert_eq!(
+            eth_to_unwrap,
+            settlement.encoder.amount_to_unwrap(weth.address())
+        );
+    }
+}
