517338fed6193bc8ea3c033c2d2e214224c3431a
Martin Beckmann <martin.beckmann@gnosis.pm>
2022-01-12 15:49:42 +0100

    Implement tests for OneInchPriceEstimator

--- crates/shared/src/price_estimation/oneinch.rs
+++ crates/shared/src/price_estimation/oneinch.rs
@@ -68,3 +68,169 @@ impl PriceEstimating for OneInchPriceEstimator {
     }
 }
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::oneinch_api::{
+        MockOneInchClient, OneInchClientImpl, RestError, SellOrderQuote, Token,
+    };
+    use reqwest::Client;
+
+    #[tokio::test]
+    async fn estimate_sell_order_succeeds() {
+        // How much GNO can you buy for 1 WETH
+        let mut one_inch = MockOneInchClient::new();
+
+        // Response was generated with:
+        //
+        // curl 'https://api.1inch.io/v4.0/1/quote?\
+        //     fromTokenAddress=0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2&\
+        //     toTokenAddress=0x6810e776880c02933d47db1b9fc05908e5386b96&\
+        //     amount=100000000000000000'
+        one_inch.expect_get_sell_order_quote().return_once(|_| {
+            Ok(RestResponse::<_>::Ok(SellOrderQuote {
+                from_token: Token {
+                    address: testlib::tokens::WETH,
+                },
+                to_token: Token {
+                    address: testlib::tokens::GNO,
+                },
+                to_token_amount: 808_069_760_400_778_577u128.into(),
+                from_token_amount: 100_000_000_000_000_000u128.into(),
+                protocols: Vec::default(),
+                estimated_gas: 189_386,
+            }))
+        });
+
+        let estimator = OneInchPriceEstimator {
+            api: Arc::new(one_inch),
+        };
+
+        let est = estimator
+            .estimate(&Query {
+                sell_token: testlib::tokens::WETH,
+                buy_token: testlib::tokens::GNO,
+                in_amount: 1_000_000_000_000_000_000u128.into(),
+                kind: OrderKind::Sell,
+            })
+            .await
+            .unwrap();
+
+        assert_eq!(est.out_amount, 808_069_760_400_778_577u128.into());
+        assert!(est.gas > 189_386.into());
+    }
+
+    #[tokio::test]
+    async fn estimating_buy_order_fails() {
+        let mut one_inch = MockOneInchClient::new();
+
+        one_inch.expect_get_sell_order_quote().times(0);
+
+        let estimator = OneInchPriceEstimator {
+            api: Arc::new(one_inch),
+        };
+
+        let est = estimator
+            .estimate(&Query {
+                sell_token: testlib::tokens::WETH,
+                buy_token: testlib::tokens::GNO,
+                in_amount: 1_000_000_000_000_000_000u128.into(),
+                kind: OrderKind::Buy,
+            })
+            .await;
+
+        assert!(matches!(
+            est,
+            Err(PriceEstimationError::UnsupportedOrderType)
+        ));
+    }
+
+    #[tokio::test]
+    async fn rest_api_errors_get_propagated() {
+        let mut one_inch = MockOneInchClient::new();
+        one_inch
+            .expect_get_sell_order_quote()
+            .times(1)
+            .return_once(|_| {
+                Ok(RestResponse::<SellOrderQuote>::Err(RestError {
+                    status_code: 500,
+                    description: "Internal Server Error".to_string(),
+                }))
+            });
+
+        let estimator = OneInchPriceEstimator {
+            api: Arc::new(one_inch),
+        };
+
+        let est = estimator
+            .estimate(&Query {
+                sell_token: testlib::tokens::WETH,
+                buy_token: testlib::tokens::GNO,
+                in_amount: 1_000_000_000_000_000_000u128.into(),
+                kind: OrderKind::Sell,
+            })
+            .await;
+
+        assert!(matches!(
+            est,
+            Err(PriceEstimationError::Other(e)) if e.to_string() == "Internal Server Error"
+        ));
+    }
+
+    #[tokio::test]
+    async fn request_errors_get_propagated() {
+        let mut one_inch = MockOneInchClient::new();
+        one_inch
+            .expect_get_sell_order_quote()
+            .times(1)
+            .return_once(|_| Err(anyhow::anyhow!("malformed JSON")));
+
+        let estimator = OneInchPriceEstimator {
+            api: Arc::new(one_inch),
+        };
+
+        let est = estimator
+            .estimate(&Query {
+                sell_token: testlib::tokens::WETH,
+                buy_token: testlib::tokens::GNO,
+                in_amount: 1_000_000_000_000_000_000u128.into(),
+                kind: OrderKind::Sell,
+            })
+            .await;
+
+        assert!(matches!(
+            est,
+            Err(PriceEstimationError::Other(e)) if e.to_string() == "malformed JSON"
+        ));
+    }
+
+    #[tokio::test]
+    #[ignore]
+    async fn real_estimate() {
+        let weth = testlib::tokens::WETH;
+        let gno = testlib::tokens::GNO;
+
+        let estimator = OneInchPriceEstimator {
+            api: Arc::new(
+                OneInchClientImpl::new(OneInchClientImpl::DEFAULT_URL, Client::new()).unwrap(),
+            ),
+        };
+
+        let result = estimator
+            .estimate(&Query {
+                sell_token: weth,
+                buy_token: gno,
+                in_amount: 10u128.pow(18).into(),
+                kind: OrderKind::Sell,
+            })
+            .await;
+
+        dbg!(&result);
+        let estimate = result.unwrap();
+        println!(
+            "1 WETH buys {} GNO, costing {} gas",
+            estimate.out_amount.to_f64_lossy() / 1e18,
+            estimate.gas,
+        );
+    }
+}
