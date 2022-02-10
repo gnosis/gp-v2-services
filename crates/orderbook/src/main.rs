b0ecce773ccff00481501fd11e4bedc735bd9058
Martin Beckmann <martin.beckmann@gnosis.pm>
2022-02-10 11:18:03 +0100

    Add metrics for native price cache hits/misses (#1638)

--- crates/orderbook/src/main.rs
+++ crates/orderbook/src/main.rs
@@ -533,6 +533,7 @@ async fn main() {
             native_token_price_estimation_amount,
         )),
         args.native_price_cache_max_age_secs,
+        metrics.clone(),
     ));
     native_price_estimator.spawn_maintenance_task(
         Duration::from_secs(1),
