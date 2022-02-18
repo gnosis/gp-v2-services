gnosis/gp-v2-services

commit a2a47605f1a268aac7084b6e85615d7d0048c591

Use new auction endpoint for solving (#1655)
This PR makes the solver use the new `v1/auction` endpoint as well as prices from the orderbook for settlement competition. These prices are used instead of the prices that used to be computed internally in the solver and result in quite a bit of removed code (🎉).

Of note, the `augument_prices` preprocessing step has been removed (this was briefly discussed in #1649). The rationale for this is that augmenting prices should no longer be needed since we used to be able to accept orders in the orderbook without a baseline price (because the orderbook uses a different set of estimators). Because the `estimated_prices` computed in the driver came from baseline prices, this caused some issues where orders could be added to the orderbook but not be considered by the driver. To remedy this, the `augment_prices` preprocessing step was added to augment the `external_prices` vector under certain conditions - allowing these orders to be traded. Since the external price vector comes from the orderbook now (and uses the same set of native prices used for computing fees when adding orders - i.e. they should always exist), it is no longer needed.

### Test Plan

CI. I will additionally run manual tests and update the PR once they are done. One concern that I thought about that I want to make sure it works are ETH/WETH orders (I don't remember seeing any special paths in the native price estimation to handle them and want to make sure that they don't get filtered out).

### Release Notes

Since this relies on new APIs that aren't yet in production, this PR will break the shadow solver until next release.

crates/model/src/auction.rs
@@ -8,7 +8,7 @@ use std::collections::BTreeMap;
 
 /// A batch auction.
 #[serde_as]
-#[derive(Clone, Debug, Deserialize, Serialize, PartialEq)]
+#[derive(Clone, Debug, Default, Deserialize, PartialEq, Serialize)]
 pub struct Auction {
     /// The block that this auction is valid for.
     /// The block number for the auction. Orders and prices are guaranteed to be

crates/orderbook/src/main.rs
@@ -20,7 +20,7 @@ use orderbook::{
     solvable_orders::SolvableOrdersCache,
     verify_deployed_contract_constants,
 };
-use primitive_types::H160;
+use primitive_types::{H160, U256};
 use shared::oneinch_api::OneInchClientImpl;
 use shared::price_estimation::native::NativePriceEstimator;
 use shared::price_estimation::native_price_cache::CachingNativePriceEstimator;
@@ -118,27 +118,27 @@ struct Arguments {
 
     /// List of token addresses to be ignored throughout service
     #[clap(long, env, use_delimiter = true)]
-    pub unsupported_tokens: Vec<H160>,
+    unsupported_tokens: Vec<H160>,
 
     /// List of account addresses to be denied from order creation
     #[clap(long, env, use_delimiter = true)]
-    pub banned_users: Vec<H160>,
+    banned_users: Vec<H160>,
 
     /// List of token addresses that should be allowed regardless of whether the bad token detector
     /// thinks they are bad. Base tokens are automatically allowed.
     #[clap(long, env, use_delimiter = true)]
-    pub allowed_tokens: Vec<H160>,
+    allowed_tokens: Vec<H160>,
 
     /// The number of pairs that are automatically updated in the pool cache.
     #[clap(long, env, default_value = "200")]
-    pub pool_cache_lru_size: usize,
+    pool_cache_lru_size: usize,
 
     /// Enable pre-sign orders. Pre-sign orders are accepted into the database without a valid
     /// signature, so this flag allows this feature to be turned off if malicious users are
     /// abusing the database by inserting a bunch of order rows that won't ever be valid.
     /// This flag can be removed once DDoS protection is implemented.
     #[clap(long, env, parse(try_from_str), default_value = "false")]
-    pub enable_presign_orders: bool,
+    enable_presign_orders: bool,
 
     /// If solvable orders haven't been successfully update in this time in seconds attempting
     /// to get them errors and our liveness check fails.
@@ -204,6 +204,16 @@ struct Arguments {
     /// Which estimators to use to estimate token prices in terms of the chain's native token.
     #[clap(long, env, default_value = "Baseline", arg_enum, use_delimiter = true)]
     native_price_estimators: Vec<PriceEstimatorType>,
+
+    /// The amount in native tokens atoms to use for price estimation. Should be reasonably large so
+    /// that small pools do not influence the prices. If not set a reasonable default is used based
+    /// on network id.
+    #[clap(
+        long,
+        env,
+        parse(try_from_str = U256::from_dec_str)
+    )]
+    amount_to_estimate_prices_with: Option<U256>,
 
     #[clap(long, env, default_value = "Baseline", arg_enum, use_delimiter = true)]
     price_estimators: Vec<PriceEstimatorType>,
@@ -276,9 +286,8 @@ async fn main() {
     let network_name = network_name(&network, chain_id);
 
     let native_token_price_estimation_amount = args
-        .shared
         .amount_to_estimate_prices_with
-        .or_else(|| shared::arguments::default_amount_to_estimate_prices_with(&network))
+        .or_else(|| default_amount_to_estimate_prices_with(&network))
         .expect("No amount to estimate prices with set.");
 
     let vault = match BalancerV2Vault::deployed(&web3).await {
@@ -721,6 +730,16 @@ fn parse_partner_fee_factor(s: &str) -> Result<HashMap<AppId, f64>> {
         res.insert(key, value);
     }
     Ok(res)
+}
+
+fn default_amount_to_estimate_prices_with(network_id: &str) -> Option<U256> {
+    match network_id {
+        // Mainnet, Rinkeby
+        "1" | "4" => Some(10u128.pow(18).into()),
+        // Xdai
+        "100" => Some(10u128.pow(21).into()),
+        _ => None,
+    }
 }
 
 #[cfg(test)]

crates/solver/src/in_flight_orders.rs
@@ -1,7 +1,4 @@
-use model::{
-    order::{Order, OrderUid},
-    SolvableOrders,
-};
+use model::{auction::Auction, order::OrderUid};
 use std::collections::{BTreeMap, HashSet};
 
 /// After a settlement transaction we need to keep track of in flight orders until the api has
@@ -15,10 +12,10 @@ pub struct InFlightOrders {
 
 impl InFlightOrders {
     /// Takes note of the new set of solvable orders and returns the ones that aren't in flight.
-    pub fn update_and_filter(&mut self, new: SolvableOrders) -> Vec<Order> {
+    pub fn update_and_filter(&mut self, auction: &mut Auction) {
         // If api has seen block X then trades starting at X + 1 are still in flight.
-        self.in_flight = self.in_flight.split_off(&(new.latest_settlement_block + 1));
-        let mut orders = new.orders;
+        self.in_flight = self.in_flight.split_off(&(auction.block + 1));
+
         // TODO - could model inflight_trades as HashMap<OrderUid, Vec<Trade>>
         // https://github.com/gnosis/gp-v2-services/issues/673
         // Note that this will result in simulation error "GPv2: order filled" if the
@@ -29,11 +26,10 @@ impl InFlightOrders {
             .flatten()
             .copied()
             .collect::<HashSet<_>>();
-        orders.retain(|order| {
+        auction.orders.retain(|order| {
             order.order_creation.partially_fillable
                 || !in_flight.contains(&order.order_meta_data.uid)
         });
-        orders
     }
 
     pub fn mark_settled_orders(&mut self, block: u64, orders: impl Iterator<Item = OrderUid>) {
@@ -44,6 +40,7 @@ impl InFlightOrders {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use model::order::Order;
 
     #[test]
     fn test() {
@@ -54,20 +51,27 @@ mod tests {
         order0.order_creation.partially_fillable = true;
         let mut order1 = Order::default();
         order1.order_meta_data.uid = OrderUid::from_integer(1);
-        let mut solvable_orders = SolvableOrders {
+        let mut auction = Auction {
+            block: 0,
             orders: vec![order0, order1],
-            latest_settlement_block: 0,
+            ..Default::default()
         };
 
-        let filtered = inflight.update_and_filter(solvable_orders.clone());
+        let mut update_and_get_filtered_orders = |auction: &Auction| {
+            let mut auction = auction.clone();
+            inflight.update_and_filter(&mut auction);
+            auction.orders
+        };
+
+        let filtered = update_and_get_filtered_orders(&auction);
         assert_eq!(filtered.len(), 2);
 
-        solvable_orders.orders[0].order_creation.partially_fillable = false;
-        let filtered = inflight.update_and_filter(solvable_orders.clone());
+        auction.orders[0].order_creation.partially_fillable = false;
+        let filtered = update_and_get_filtered_orders(&auction);
         assert_eq!(filtered.len(), 1);
 
-        solvable_orders.latest_settlement_block = 1;
-        let filtered = inflight.update_and_filter(solvable_orders);
+        auction.block = 1;
+        let filtered = update_and_get_filtered_orders(&auction);
         assert_eq!(filtered.len(), 2);
     }
 }

crates/solver/src/main.rs
@@ -9,16 +9,12 @@ use shared::{
     maintenance::{Maintaining, ServiceMaintenance},
     metrics::{serve_metrics, setup_metrics_registry},
     network::network_name,
-    price_estimation::baseline::BaselinePriceEstimator,
     recent_block_cache::CacheConfig,
     sources::{
         self,
         balancer_v2::{pool_fetching::BalancerContracts, BalancerFactoryKind, BalancerPoolFetcher},
-        uniswap_v2::{
-            pool_cache::PoolCache,
-            pool_fetching::{PoolFetcher, PoolFetching},
-        },
-        BaselineSource, PoolAggregator,
+        uniswap_v2::{pool_cache::PoolCache, pool_fetching::PoolFetcher},
+        BaselineSource,
     },
     token_info::{CachedTokenInfoFetcher, TokenInfoFetcher},
     token_list::TokenList,
@@ -361,12 +357,6 @@ async fn main() {
         &args.shared.base_tokens,
     ));
 
-    let native_token_price_estimation_amount = args
-        .shared
-        .amount_to_estimate_prices_with
-        .or_else(|| shared::arguments::default_amount_to_estimate_prices_with(&network_id))
-        .expect("No amount to estimate prices with set.");
-
     let token_info_fetcher = Arc::new(CachedTokenInfoFetcher::new(Box::new(TokenInfoFetcher {
         web3: web3.clone(),
     })));
@@ -420,13 +410,6 @@ async fn main() {
             })
             .collect();
 
-    let pool_aggregator = Arc::new(PoolAggregator {
-        pool_fetchers: pool_caches
-            .values()
-            .map(|cache| cache.clone() as Arc<dyn PoolFetching>)
-            .collect(),
-    });
-
     let (balancer_pool_maintainer, balancer_v2_liquidity) =
         if baseline_sources.contains(&BaselineSource::BalancerV2) {
             let contracts = BalancerContracts::new(&web3).await.unwrap();
@@ -461,13 +444,6 @@ async fn main() {
             (None, None)
         };
 
-    let price_estimator = Arc::new(BaselinePriceEstimator::new(
-        pool_aggregator,
-        gas_price_estimator.clone(),
-        base_tokens.clone(),
-        native_token_contract.address(),
-        native_token_price_estimation_amount,
-    ));
     let uniswap_like_liquidity = build_amm_artifacts(
         &pool_caches,
         settlement_contract.clone(),
@@ -624,7 +600,6 @@ async fn main() {
     let mut driver = Driver::new(
         settlement_contract,
         liquidity_collector,
-        price_estimator,
         solver,
         gas_price_estimator,
         args.settle_interval,
@@ -638,7 +613,6 @@ async fn main() {
         market_makable_token_list,
         current_block_stream.clone(),
         solution_submitter,
-        native_token_price_estimation_amount,
         args.max_settlements_per_solver,
         api,
         order_converter,

crates/solver/src/orderbook.rs
@@ -1,4 +1,5 @@
-use model::SolvableOrders;
+use anyhow::Result;
+use model::auction::Auction;
 use reqwest::{Client, Url};
 
 pub struct OrderBookApi {
@@ -12,11 +13,10 @@ impl OrderBookApi {
         Self { base, client }
     }
 
-    pub async fn get_orders(&self) -> reqwest::Result<SolvableOrders> {
-        const PATH: &str = "/api/v2/solvable_orders";
-        let mut url = self.base.clone();
-        url.set_path(PATH);
-        self.client.get(url).send().await?.json().await
+    pub async fn get_auction(&self) -> Result<Auction> {
+        let url = self.base.join("api/v1/auction")?;
+        let auction = self.client.get(url).send().await?.json().await?;
+        Ok(auction)
     }
 }
 
@@ -24,12 +24,24 @@ impl OrderBookApi {
 pub mod test_util {
     use super::*;
 
+    // cargo test local_orderbook -- --ignored --nocapture
+    #[tokio::test]
+    #[ignore]
+    async fn local_orderbook() {
+        let api = OrderBookApi::new(Url::parse("http://localhost:8080").unwrap(), Client::new());
+        let auction = api.get_auction().await.unwrap();
+        println!("{:#?}", auction);
+    }
+
     // cargo test real_orderbook -- --ignored --nocapture
     #[tokio::test]
     #[ignore]
     async fn real_orderbook() {
-        let api = OrderBookApi::new(Url::parse("http://localhost:8080").unwrap(), Client::new());
-        let orders = api.get_orders().await.unwrap();
-        println!("{:?}", orders);
+        let api = OrderBookApi::new(
+            Url::parse("https://barn.api.cow.fi/mainnet/").unwrap(),
+            Client::new(),
+        );
+        let auction = api.get_auction().await.unwrap();
+        println!("{:#?}", auction);
     }
 }

crates/e2e/tests/e2e/eth_integration.rs
@@ -107,10 +107,10 @@ async fn eth_integration(web3: Web3) {
 
     let OrderbookServices {
         maintenance,
-        price_estimator,
         block_stream,
         solvable_orders_cache,
         base_tokens,
+        ..
     } = OrderbookServices::new(&web3, &contracts).await;
 
     let client = reqwest::Client::new();
@@ -205,7 +205,6 @@ async fn eth_integration(web3: Web3) {
     let mut driver = solver::driver::Driver::new(
         contracts.gp_settlement.clone(),
         liquidity_collector,
-        price_estimator,
         vec![solver],
         Arc::new(web3.clone()),
         Duration::from_secs(30),
@@ -234,7 +233,6 @@ async fn eth_integration(web3: Web3) {
                 }),
             ],
         },
-        1_000_000_000_000_000_000_u128.into(),
         10,
         create_orderbook_api(),
         create_order_converter(&web3, contracts.weth.address()),
@@ -263,6 +261,6 @@ async fn eth_integration(web3: Web3) {
     maintenance.run_maintenance().await.unwrap();
     solvable_orders_cache.update(0).await.unwrap();
 
-    let orders = create_orderbook_api().get_orders().await.unwrap();
-    assert!(orders.orders.is_empty());
+    let auction = create_orderbook_api().get_auction().await.unwrap();
+    assert!(auction.orders.is_empty());
 }

crates/e2e/tests/e2e/onchain_settlement.rs
@@ -135,11 +135,11 @@ async fn onchain_settlement(web3: Web3) {
 
     // Place Orders
     let OrderbookServices {
-        price_estimator,
         maintenance,
         block_stream,
         solvable_orders_cache,
         base_tokens,
+        ..
     } = OrderbookServices::new(&web3, &contracts).await;
 
     let client = reqwest::Client::new();
@@ -210,7 +210,6 @@ async fn onchain_settlement(web3: Web3) {
     let mut driver = solver::driver::Driver::new(
         contracts.gp_settlement.clone(),
         liquidity_collector,
-        price_estimator,
         vec![solver],
         Arc::new(web3.clone()),
         Duration::from_secs(30),
@@ -239,7 +238,6 @@ async fn onchain_settlement(web3: Web3) {
                 }),
             ],
         },
-        1_000_000_000_000_000_000_u128.into(),
         10,
         create_orderbook_api(),
         create_order_converter(&web3, contracts.weth.address()),
@@ -268,8 +266,8 @@ async fn onchain_settlement(web3: Web3) {
     maintenance.run_maintenance().await.unwrap();
     solvable_orders_cache.update(0).await.unwrap();
 
-    let orders = create_orderbook_api().get_orders().await.unwrap();
-    assert!(orders.orders.is_empty());
+    let auction = create_orderbook_api().get_auction().await.unwrap();
+    assert!(auction.orders.is_empty());
 
     // Drive again to ensure we can continue solution finding
     driver.single_run().await.unwrap();

crates/e2e/tests/e2e/settlement_without_onchain_liquidity.rs
@@ -138,11 +138,11 @@ async fn onchain_settlement_without_liquidity(web3: Web3) {
 
     // Place Orders
     let OrderbookServices {
-        price_estimator,
         maintenance,
         block_stream,
         solvable_orders_cache,
         base_tokens,
+        ..
     } = OrderbookServices::new(&web3, &contracts).await;
 
     let client = reqwest::Client::new();
@@ -199,7 +199,6 @@ async fn onchain_settlement_without_liquidity(web3: Web3) {
     let mut driver = solver::driver::Driver::new(
         contracts.gp_settlement.clone(),
         liquidity_collector,
-        price_estimator,
         vec![solver],
         Arc::new(web3.clone()),
         Duration::from_secs(30),
@@ -228,7 +227,6 @@ async fn onchain_settlement_without_liquidity(web3: Web3) {
                 }),
             ],
         },
-        1_000_000_000_000_000_000_u128.into(),
         10,
         create_orderbook_api(),
         create_order_converter(&web3, contracts.weth.address()),
@@ -265,8 +263,8 @@ async fn onchain_settlement_without_liquidity(web3: Web3) {
     maintenance.run_maintenance().await.unwrap();
     solvable_orders_cache.update(0).await.unwrap();
 
-    let orders = create_orderbook_api().get_orders().await.unwrap();
-    assert!(orders.orders.is_empty());
+    let auction = create_orderbook_api().get_auction().await.unwrap();
+    assert!(auction.orders.is_empty());
 
     // Drive again to ensure we can continue solution finding
     driver.single_run().await.unwrap();

commit 1c1df869eb97bb44c747736f4a909851e4d8c928

Add latest settlement block to Auction model (#1673)
This PR fixes a regression introduced with #1655.

Specifically, the `/auction` endpoint assumed that once the `event_updater` had finished processing a new block that the solvable orders cache would have already been updated. However:
- These two components update on different schedules so this is not true - this caused the returned `Auction` model to say it was on block `X` (which, for example, could include a new settlement) even though the solvable orders had not been updated on that block.
- Updates on new blocks are racy. This means that the solvable orders may process block `X` _while_ the event handler is waiting on events for the block from the node, causing the database to still reflect block `X-1` instead, meaning that it did not "truly" update to block `X` as it used stale settlement data and may include filled orders in the `solvable_orders` collection.

This PR does two things:
- Request updating the solvable order cache on each new block. This ensures that user balances are as up to date as possible.
- Re-introduces the `latest_settlement_block` to the `Auction` type as a reliable way to determine which orders may be considered "in flight" or not.

Ultimately, there is a bit of a "data synchronization" issue that is hard to solve here - we get data from two places (current block number and balances from the block chain and settlements from the DB) and they aren't necessarily in sync when updating the solvable orders cache.

Furthermore, #1655 introduced `EventUpdating` for getting the latest handled block. Since we don't use it anymore I went ahead and reverted that part of the change to clean things up a bit.

Note that I didn't bump the API version from `v1/auction` to `v2/auction` since it is still in staging.

### Test Plan

CI and existing unit tests.

crates/model/src/auction.rs
@@ -9,11 +9,18 @@ use std::collections::BTreeMap;
 /// A batch auction.
 #[serde_as]
 #[derive(Clone, Debug, Default, Deserialize, PartialEq, Serialize)]
+#[serde(rename_all = "camelCase")]
 pub struct Auction {
     /// The block that this auction is valid for.
     /// The block number for the auction. Orders and prices are guaranteed to be
     /// valid on this block.
     pub block: u64,
+
+    /// The latest block on which a settlement has been processed.
+    ///
+    /// Note that under certain conditions it is possible for a settlement to
+    /// have been mined as part of [`block`] but not have yet been processed.
+    pub latest_settlement_block: u64,
 
     /// The solvable orders included in the auction.
     pub orders: Vec<Order>,
@@ -41,6 +48,7 @@ mod tests {
         };
         let auction = Auction {
             block: 42,
+            latest_settlement_block: 40,
             orders: vec![order(1), order(2)],
             prices: btreemap! {
                 H160([2; 20]) => U256::from(2),
@@ -52,6 +60,7 @@ mod tests {
             serde_json::to_value(&auction).unwrap(),
             json!({
                 "block": 42,
+                "latestSettlementBlock": 40,
                 "orders": [
                     order(1),
                     order(2),

crates/orderbook/openapi.yml
@@ -679,6 +679,13 @@ components:
           description: |
             The block number for the auction. Orders and prices are guaranteed to be valid on this
             block. Proposed settlements should be valid for this block as well.
+        latestSettlementBlock:
+          type: integer
+          description: |
+            The latest block on which a settlement has been processed.
+
+            Note that under certain conditions it is possible for a settlement to have been mined as
+            part of `block` but not have yet been processed.
         orders:
           type: array
           items:

crates/orderbook/src/event_updater.rs
@@ -11,14 +11,6 @@ use shared::{
 };
 use tokio::sync::Mutex;
 
-/// A trait for abstracting event updater methods.
-#[async_trait::async_trait]
-pub trait EventUpdating: Send + Sync + 'static {
-    /// Returns the last handled block number. Events are guaranteed to be
-    /// processed up until this block.
-    async fn last_handled_block(&self) -> Option<u64>;
-}
-
 pub struct EventUpdater<Database: EventStoring<ContractEvent>>(
     Mutex<EventHandler<DynWeb3, GPv2SettlementContract, Database>>,
 );
@@ -38,16 +30,6 @@ where
             db,
             start_sync_at_block,
         )))
-    }
-}
-
-#[async_trait::async_trait]
-impl<Database> EventUpdating for EventUpdater<Database>
-where
-    Database: EventStoring<ContractEvent> + Send + Sync + 'static,
-{
-    async fn last_handled_block(&self) -> Option<u64> {
-        self.0.lock().await.last_handled_block()
     }
 }
 

crates/orderbook/src/main.rs
@@ -618,14 +618,18 @@ async fn main() {
         bad_token_detector,
         native_price_estimator,
         args.enable_presign_orders,
-        solvable_orders_cache,
+        solvable_orders_cache.clone(),
         args.solvable_orders_max_update_age,
         order_validator.clone(),
-        event_updater.clone(),
         metrics.clone(),
     ));
     let mut service_maintainer = ServiceMaintenance {
-        maintainers: vec![database.clone(), event_updater, pool_fetcher],
+        maintainers: vec![
+            database.clone(),
+            event_updater,
+            pool_fetcher,
+            solvable_orders_cache,
+        ],
     };
     if let Some(balancer) = balancer_pool_fetcher {
         service_maintainer.maintainers.push(balancer);

crates/orderbook/src/orderbook.rs
@@ -1,7 +1,6 @@
 use crate::{
     api::order_validation::{OrderValidating, OrderValidator, ValidationError},
     database::orders::{InsertionError, OrderFilter, OrderStoring},
-    event_updater::EventUpdating,
     solvable_orders::{SolvableOrders, SolvableOrdersCache},
 };
 use anyhow::{ensure, Context, Result};
@@ -85,7 +84,6 @@ pub struct Orderbook {
     solvable_orders: Arc<SolvableOrdersCache>,
     solvable_orders_max_update_age: Duration,
     order_validator: Arc<OrderValidator>,
-    event_updater: Arc<dyn EventUpdating>,
     metrics: Arc<dyn OrderbookMetrics>,
 }
 
@@ -105,7 +103,6 @@ impl Orderbook {
         solvable_orders: Arc<SolvableOrdersCache>,
         solvable_orders_max_update_age: Duration,
         order_validator: Arc<OrderValidator>,
-        event_updater: Arc<dyn EventUpdating>,
         metrics: Arc<dyn OrderbookMetrics>,
     ) -> Self {
         Self {
@@ -118,7 +115,6 @@ impl Orderbook {
             solvable_orders,
             solvable_orders_max_update_age,
             order_validator,
-            event_updater,
             metrics,
         }
     }
@@ -244,21 +240,18 @@ impl Orderbook {
     }
 
     pub async fn get_auction(&self) -> Result<Auction> {
-        let last_handled_block = self.event_updater.last_handled_block().await;
         let solvable_orders = self.get_solvable_orders().await?;
 
         let order_count = solvable_orders.orders.len();
-
-        let block = last_handled_block.unwrap_or(solvable_orders.latest_settlement_block);
         let (orders, prices) =
             get_orders_with_native_prices(solvable_orders.orders, &*self.native_price_estimator)
                 .await;
-
         let filtered_orders = order_count - orders.len();
         self.metrics.filtered_solvable_orders(filtered_orders);
 
         Ok(Auction {
-            block,
+            block: solvable_orders.block,
+            latest_settlement_block: solvable_orders.latest_settlement_block,
             orders,
             prices,
         })

crates/orderbook/src/solvable_orders.rs
@@ -7,7 +7,8 @@ use anyhow::Result;
 use model::order::Order;
 use primitive_types::U256;
 use shared::{
-    bad_token::BadTokenDetecting, current_block::CurrentBlockStream, time::now_in_epoch_seconds,
+    bad_token::BadTokenDetecting, current_block::CurrentBlockStream, maintenance::Maintaining,
+    time::now_in_epoch_seconds,
 };
 use std::{
     collections::{HashMap, HashSet},
@@ -37,7 +38,6 @@ type Balances = HashMap<Query, U256>;
 struct Inner {
     orders: SolvableOrders,
     balances: Balances,
-    block: u64,
 }
 
 #[derive(Clone, Debug)]
@@ -45,6 +45,7 @@ pub struct SolvableOrders {
     pub orders: Vec<Order>,
     pub update_time: Instant,
     pub latest_settlement_block: u64,
+    pub block: u64,
 }
 
 impl SolvableOrdersCache {
@@ -66,9 +67,9 @@ impl SolvableOrdersCache {
                     orders: Vec::new(),
                     update_time: Instant::now(),
                     latest_settlement_block: 0,
+                    block: 0,
                 },
                 balances: Default::default(),
-                block: 0,
             }),
         });
         tokio::task::spawn(update_task(Arc::downgrade(&self_), current_block));
@@ -102,7 +103,7 @@ impl SolvableOrdersCache {
         // cannot have changed.
         let old_balances = {
             let inner = self.cache.lock().unwrap();
-            if inner.block == block {
+            if inner.orders.block == block {
                 inner.balances.clone()
             } else {
                 HashMap::new()
@@ -138,9 +139,9 @@ impl SolvableOrdersCache {
                 orders,
                 update_time: Instant::now(),
                 latest_settlement_block: db_solvable_orders.latest_settlement_block,
+                block,
             },
             balances: new_balances,
-            block,
         };
 
         Ok(())
@@ -241,6 +242,14 @@ async fn update_task(cache: Weak<SolvableOrdersCache>, current_block: CurrentBlo
             Ok(()) => tracing::debug!("updated solvable orders"),
             Err(err) => tracing::error!(?err, "failed to update solvable orders"),
         }
+    }
+}
+
+#[async_trait::async_trait]
+impl Maintaining for SolvableOrdersCache {
+    async fn run_maintenance(&self) -> Result<()> {
+        self.request_update();
+        Ok(())
     }
 }
 

crates/solver/src/in_flight_orders.rs
@@ -14,7 +14,9 @@ impl InFlightOrders {
     /// Takes note of the new set of solvable orders and returns the ones that aren't in flight.
     pub fn update_and_filter(&mut self, auction: &mut Auction) {
         // If api has seen block X then trades starting at X + 1 are still in flight.
-        self.in_flight = self.in_flight.split_off(&(auction.block + 1));
+        self.in_flight = self
+            .in_flight
+            .split_off(&(auction.latest_settlement_block + 1));
 
         // TODO - could model inflight_trades as HashMap<OrderUid, Vec<Trade>>
         // https://github.com/gnosis/gp-v2-services/issues/673
@@ -71,6 +73,10 @@ mod tests {
         assert_eq!(filtered.len(), 1);
 
         auction.block = 1;
+        let filtered = update_and_get_filtered_orders(&auction);
+        assert_eq!(filtered.len(), 1);
+
+        auction.latest_settlement_block = 1;
         let filtered = update_and_get_filtered_orders(&auction);
         assert_eq!(filtered.len(), 2);
     }

crates/e2e/tests/e2e/services.rs
@@ -185,7 +185,6 @@ impl OrderbookServices {
             solvable_orders_cache.clone(),
             Duration::from_secs(600),
             order_validator.clone(),
-            event_updater.clone(),
             Arc::new(NoopMetrics),
         ));
         let maintenance = ServiceMaintenance {