29961cb21075168c8830aa1f714b8b962565ed5f
Martin Beckmann <martin.beckmann@gnosis.pm>
2022-03-11 10:23:40 +0100

    Use actual 0x trade gas cost

--- crates/shared/src/price_estimation/gas.rs
+++ crates/shared/src/price_estimation/gas.rs
@@ -46,8 +46,8 @@ pub static GAS_PER_ORDER: u64 = 66_315;
 pub static GAS_PER_UNISWAP: u64 = 94_696;
 
 /// lower bound for execution of one trade on zeroex
-// TODO this is currently a placeholder and needs to be replaced with the real value
-pub static GAS_PER_ZEROEX_ORDER: u64 = 94_696;
+// estimated with https://dune.xyz/queries/492900/933779
+pub static GAS_PER_ZEROEX_ORDER: u64 = 134_563;
 
 /// lower bound for executing one trade on balancer
 ///
