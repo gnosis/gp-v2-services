5bd090b9acd40c501118bca43fc820a4f0343fcf
Martin Beckmann <martin.beckmann@gnosis.pm>
2022-03-21 10:56:31 +0100

    More idiomatic lable creation

--- crates/solver/src/settlement_submission/submitter/custom_nodes_api.rs
+++ crates/solver/src/settlement_submission/submitter/custom_nodes_api.rs
@@ -53,7 +53,7 @@ impl TransactionSubmitting for CustomNodesApi {
 
         loop {
             let (result, index, rest) = futures::future::select_all(futures).await;
-            let lable = "custom_nodes_".to_string() + &index.to_string();
+            let lable = format!("custom_nodes_{}", index);
             match result {
                 Ok(tx_hash) => {
                     super::track_submission_success(lable.as_str(), true);
